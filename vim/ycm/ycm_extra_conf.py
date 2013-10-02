531f9a626970be6bdbda0291e98329958f28ebce
Robert Kernan <rkernan@gmail.com>
2013-10-02 00:08:16 -0400

    Fixed some weird naming and formatting.

--- vim/ycm/ycm_extra_conf.py
+++ vim/ycm/ycm_extra_conf.py
@@ -2,43 +2,55 @@ import os
 import platform
 import vim
 
+
+# Convert a relative path to an abosolute path.
 def AbsolutePath(relPath):
     return os.path.join(os.getcwd(), relPath)
 
+
+# Find a directory Upwards from the current directory.
+def FindDirUp(dirName, toRoot=False):
+    # TODO
+    return [""]
+
+
+# Get the platform name.
 def GetPlatformName():
     system = platform.system()
     if system == "Linux":
         return "LINUX"
     elif system == "Windows":
         return "WIN32"
-    raise Exception("Unknow platform.")
+    raise Exception("Unknown platform.")
 
+
+# Create filetype flags.
 def FlagsForFile(filename):
     filetype = vim.eval("&filetype")
     if filetype == "c":
         flags = [
-                "-std=c11",
-                "-x", "c"
-                "-Wall",
-                "-Wextra",
-                "-I", AbsolutePath("inc"),
-                "-I", AbsolutePath("include"),
-                "-DDEBUG",
-                ]
+            "-std=c11",
+            "-x", "c"
+            "-Wall",
+            "-Wextra",
+            "-I", AbsolutePath("inc"),
+            "-I", AbsolutePath("include"),
+            "-DDEBUG",
+            ]
         try:
             flags += ["-D%s" % GetPlatformName()]
         except:
             pass
     elif filetype == "cpp":
         flags = [
-                "-std=c++11",
-                "-x", "c++",
-                "-Wall",
-                "-Wextra",
-                "-I", AbsolutePath("inc"),
-                "-I", AbsolutePath("include"),
-                "-DDEBUG",
-                ]
+            "-std=c++11",
+            "-x", "c++",
+            "-Wall",
+            "-Wextra",
+            "-I", AbsolutePath("inc"),
+            "-I", AbsolutePath("include"),
+            "-DDEBUG",
+            ]
         try:
             flags += ["-D%s" % GetPlatformName()]
         except:
