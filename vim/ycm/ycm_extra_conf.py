7c765465dcdc65d70b4ff79083ce81132cc6c2e9
Robert Kernan <rkernan@gmail.com>
2013-10-25 17:20:56 -0400

    Added more c++ file extensions.

--- vim/ycm/ycm_extra_conf.py
+++ vim/ycm/ycm_extra_conf.py
@@ -1,17 +1,12 @@
 import os
 import platform
+# FIXME Can't import vim since commit on 2013-10-24 (change to client-server).
 # import vim
 
 
 # Convert a relative path to an abosolute path.
 def AbsolutePath(relPath):
     return os.path.join(os.getcwd(), relPath)
-
-
-# Find a directory Upwards from the current directory.
-def FindDirUp(dirName, toRoot=False):
-    # TODO
-    return [""]
 
 
 # Get the platform name.
@@ -24,19 +19,20 @@ def GetPlatformName():
     raise Exception("Unknown platform.")
 
 
+# FIXME
 def GetFiletype(filename):
-    # TODO Nasty hack. Remove ASAP.
     extension = os.path.splitext(filename)[1][1:].strip().lower()
     if extension == 'c' or extension == 'h':
         return 'c'
-    elif extension == 'cpp' or extension == 'hpp' or \
-            extension == 'cc' or extension == 'hh':
+    elif extension == 'cpp' or extension == 'cxx' or extension == 'cc'        \
+            or extension == 'hpp' or extension == 'hxx' or extension == 'hh'  \
+            or extension == 'inl':
         return 'cpp'
 
 
 # Create filetype flags.
 def FlagsForFile(filename):
-    # TODO After the new YCM update (2013-10-25) `import vim` cannot be done.
+    # FIXME
     # filetype = vim.eval('&filetype')
     filetype = GetFiletype(filename)
     if filetype == "c":
