dc61066de534935c5cc50e844f2aa4e2da18d6a5
Robert Kernan <rkernan@gmail.com>
2017-06-28 20:03:53 -0400

    Moved vimrc into .vim. Increased gvim font size

--- install_win.bat
+++ install_win.bat
@@ -1,7 +1,5 @@
 rmdir /s /q "%USERPROFILE%\vimfiles"
-del "%USERPROFILE%\_vimrc"
 mklink /j "%USERPROFILE%\vimfiles" "vim\.vim"
-copy "vim\.vimrc" "%USERPROFILE%\_vimrc"
 
 del "%USERPROFILE%\.gitconfig"
 copy "git\.gitconfig" "%USERPROFILE%\.gitconfig"
