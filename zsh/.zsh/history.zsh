e2e9fdb4a07070e46bcf975078afb858d5cf5e09
Robert Kernan <rkernan@gmail.com>
2017-07-20 14:30:23 -0400

    Removed history-substring-search

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -17,25 +17,3 @@ setopt HIST_FIND_NO_DUPS      # do not display duplicates
 setopt HIST_IGNORE_SPACE      # do not record an event that starts with a space
 setopt HIST_SAVE_NO_DUPS      # do not write a duplicate event to the history file
 setopt HIST_VERIFY            # do not execute immediately on history expansion
-
-# source history-substring-search
-source "${0:h}/history-substring-search/zsh-history-substring-search.zsh" || return 1
-
-# history-substring-search key bindings
-if [[ -n "$key_info" ]]; then
-	# emacs
-	bindkey -M emacs "$key_info[Control]P" history-substring-search-up
-	bindkey -M emacs "$key_info[Control]N" history-substring-search-down
-
-	# vi
-	bindkey -M vicmd "k" history-substring-search-up
-	bindkey -M vicmd "j" history-substring-search-down
-	bindkey -M viins "$key_info[Control]P" history-substring-search-up
-	bindkey -M viins "$key_info[Control]N" history-substring-search-down
-
-	# both
-	for keymap in 'emacs' 'viins'; do
-		bindkey -M "$keymap" "$key_info[Up]" history-substring-search-up
-		bindkey -M "$keymap" "$key_info[Down]" history-substring-search-down
-	done
-fi
