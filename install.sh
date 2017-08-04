2ccf0584fad0de74d674a7b80cecba7139b4f46d
Robert Kernan <rkernan@gmail.com>
2017-08-04 15:21:50 -0400

    Added install targets cli and gui.

--- install.sh
+++ install.sh
@@ -4,10 +4,34 @@ function stow {
   command stow -t $HOME -v $1
 }
 
-stow awesome
-stow git
-stow nvim
-stow termite
-stow tmux
-stow vim
-stow zsh
+function install_cli_config {
+  stow git
+  stow nvim
+  stow tmux
+  stow vim
+  stow zsh
+}
+
+function install_gui_config {
+  stow awesome
+  stow termite
+}
+
+readonly target=${1:-all}
+
+case $target in
+  all)
+    install_cli_config
+    install_gui_config
+    ;;
+  cli)
+    install_cli_config
+    ;;
+  gui)
+    install_gui_config
+    ;;
+  *)
+    echo "Unknown target $target" 2>&1
+    exit 1
+    ;;
+esac
