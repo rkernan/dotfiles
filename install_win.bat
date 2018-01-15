093a53745036d9e613ce62b22bc5ce9e4b7bf92b
Robert Kernan <rkernan@gmail.com>
2018-01-15 14:47:08 -0500

    Fixed git-config path on windows

--- install_win.bat
+++ install_win.bat
@@ -2,6 +2,6 @@ rmdir /s /q "%USERPROFILE%\AppData\Local\nvim"
 mklink /j "%USERPROFILE%\AppData\Local\nvim" "nvim\.config\nvim"
 
 del "%USERPROFILE%\.gitconfig"
-copy "git\.gitconfig" "%USERPROFILE%\.gitconfig"
+copy "git\.config\git\config" "%USERPROFILE%\.gitconfig"
 
 pause
