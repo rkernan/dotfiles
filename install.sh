1bfcae188f0a85f96cc02828e7b2dde947817e2a
Robert Kernan <rkernan@gmail.com>
2019-02-28 18:02:44 -0500

    Install neovim in user directory with appimage

--- install.sh
+++ install.sh
@@ -6,23 +6,23 @@ function stow {
   command stow -t "$HOME" -v "$1"
 }
 
-function guess_target {
+guess_target() {
   if which pacman >/dev/null 2>&1; then
     # arch linux
-    echo bin-arch cli gui pyenv
+    echo bin-arch cli gui pyenv neovim
   elif which apt-get >/dev/null 2>&1; then
     # ubuntu/linux-subsystem
-    echo bash-to-zsh cli pyenv
+    echo bash-to-zsh cli pyenv neovim
   elif which yum >/dev/null 2>&1; then
     # centos/rhel/fedora - work
-    echo bash-to-zsh cli pyenv
+    echo bin-arch cli gui pyenv neovim
   else
     # other
     echo cli pyenv
   fi
 }
 
-function install_pyenv {
+setup_pyenv() {
   # install pyenv
   readonly pyenv_root="$HOME/.pyenv"
   if [ ! -d "$pyenv_root" ]; then
@@ -42,30 +42,40 @@ function install_pyenv {
   eval "$(pyenv virtualenv-init -)"
   # update
   pyenv update
-  # setup neovim2
+}
+
+setup_neovim_venv() {
+  # setup neovim2 virtualenv
   readonly python2_ver="2.7.15"
   if [ ! -d "$(pyenv root)/versions/${python2_ver}" ]; then
     pyenv install "$python2_ver"
   fi
   readonly neovim2_env="neovim2"
-  rm -f "$(pyenv root)/versions/${neovim2_env}"
-  rm -rf "$(pyenv root)/versions/${python2_ver}/envs/${neovim2_env}"
-  pyenv virtualenv "$python2_ver" "$neovim2_env"
+  if [ ! -d "$(pyenv root)/versions/${neovim2_env}" ]; then
+    pyenv virtualenv "$python2_ver" "$neovim2_env"
+  fi
   pyenv activate "$neovim2_env"
-  pip install pynvim
-  pip install neovim
-  # setup neovim3
+  pip install -U pynvim neovim
+  # setup neovim3 virtualenv
   readonly python3_ver="3.7.2"
   if [ ! -d "$(pyenv root)/versions/${python3_ver}" ]; then
     pyenv install "$python3_ver"
   fi
   readonly neovim3_env="neovim3"
-  rm -f "$(pyenv root)/versions/${neovim3_env}"
-  rm -rf "$(pyenv root)/versions/${python3_ver}/envs/${neovim3_env}"
-  pyenv virtualenv "$python3_ver" "$neovim3_env"
+  if [ ! -d "$(pyenv root)/versions/${neovim3_env}" ]; then
+    pyenv virtualenv "$python3_ver" "$neovim3_env"
+  fi
   pyenv activate "$neovim3_env"
-  pip install pynvim
-  pip install neovim
+  pip install -U pynvim neovim
+}
+
+setup_local_neovim() {
+  pushd ~/bin
+  rm -f nvim.appimage
+  curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+  chmod u+x nvim.appimage
+  ln -fs nvim.appimage nvim
+  popd
 }
 
 readonly targets=${*:-$(guess_target)}
@@ -99,7 +109,11 @@ for target in $targets; do
       stow udiskie
       ;;
     pyenv)
-      install_pyenv
+      setup_pyenv
+      ;;
+    neovim)
+      setup_neovim_venv
+      setup_local_neovim
       ;;
     *)
       echo "Unknown target $target" 2>&1
