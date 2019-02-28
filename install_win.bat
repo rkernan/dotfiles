e67991dc02e57c65c210d1f45a9f67b83be6312d
Robert Kernan <rkernan@gmail.com>
2019-02-28 18:07:19 -0500

    Removed windows installer

--- install_win.bat
+++
@@ -1,7 +0,0 @@
-rmdir /s /q "%USERPROFILE%\AppData\Local\nvim"
-mklink /j "%USERPROFILE%\AppData\Local\nvim" "nvim\.config\nvim"
-
-del "%USERPROFILE%\.gitconfig"
-copy "git\.config\git\config" "%USERPROFILE%\.gitconfig"
-
-pause
