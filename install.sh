24611293a84eeb9b7b69e4c7236e4bdfc1bc37d1
Robert Kernan <rkernan@gmail.com>
2019-02-28 18:52:57 -0500

    Verboe go get

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ setup_language_servers() {
   pip install -U python-language-server
   pyenv deactivate
   # go
-  go get -u github.com/sourcegraph/go-langserver
+  go get -u -v github.com/sourcegraph/go-langserver
 }
 
 setup_local_neovim() {
