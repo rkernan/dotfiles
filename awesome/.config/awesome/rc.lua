a27218a7ccde7f5b76bbba5c9b58d4c08f18c776
Robert Kernan <rkernan@gmail.com>
2019-04-14 20:23:12 -0400

    Moved error handling to separate file

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,3 +1,5 @@
+require("errors")
+
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
@@ -7,27 +9,6 @@ local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 require("awful.hotkeys_popup.keys")
-
--- Handle startup errors that resulted in config fallback
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Startup Error",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Runtime Error",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
 
 -- Keys
 modkey = "Mod4"
