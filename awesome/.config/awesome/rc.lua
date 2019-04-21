a6f70d2ef83e1f643991ac2efeaff9ac13d335fb
Robert Kernan <rkernan@gmail.com>
2019-04-21 03:45:20 -0400

    Disable sloppy focus

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -289,6 +289,6 @@ client.connect_signal("focus", border_adjust)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-  c:emit_signal("request::activate", "mouse_enter", {raise = true})
-end)
+-- client.connect_signal("mouse::enter", function(c)
+--   c:emit_signal("request::activate", "mouse_enter", {raise = true})
+-- end)
