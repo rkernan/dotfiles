0b50f18377716a1ee8df8940d92067e65855326a
Robert Kernan <rkernan@gmail.com>
2019-04-20 21:54:56 -0400

    Switched to google-chrome-stable

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -16,7 +16,7 @@ modkey       = "Mod4"
 altkey       = "Mod1"
 terminal     = "termite"
 editor       = os.getenv("EDITOR") or "vim"
-browser      = "chromium"
+browser      = "google-chrome-stable"
 scrlocker    = "light-locker-command --lock"
 
 --------------------
