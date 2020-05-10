31672da8ff468e83645866358c07a00d32d1597e
Robert Kernan <rkernan@gmail.com>
2020-05-09 23:43:32 -0400

    Don't install missing packages from brew or other
    - Added `checkhealth` target that outputs all required packages and
      their full path (empty if not installed)
    - Added `install_brew` target to install linuxbrew
    - Added `install_rustup` target to install rustup and cargo
    
    The `install_brew` and `install_rustup` targets *must* be installed
    manually.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
@@ -12,71 +12,65 @@ version() {
   echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
 }
 
-command_exists() {
-  local cmd=$1
-  if [ -x "$(command -v $cmd)" ]; then
-    return 0
-  else
+check_command() {
+  local cmd="$1"
+  local exec="$(which "$cmd" 2>/dev/null || true)"
+  if [ -z "$exec" ]; then
+    (>&2 echo -e "$cmd\t->")
     return 1
+  else
+    echo -e "$cmd\t-> $exec"
+    return 0
   fi
 }
 
 guess_target() {
   if [ $(command -v pacman) ]; then
     # arch linux
-    echo cli gui
+    echo "cli gui"
   elif [ $(command -v yum) ]; then
     # centos/rhel/fedora - work
-    echo cli work
+    echo "cli work"
   else
     # other
-    echo cli
+    echo "cli"
   fi
+  # always check health
+  echo "checkhealth"
 }
 
 install_brew() {
-  if ! command_exists brew; then
+  if ! check_command brew >/dev/null 2>&1; then
     curl -sL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash -s
   else
     brew update
   fi
 }
 
-install_rust() {
-  if [ ! -e "$HOME/.rustup" ]; then
+install_rustup() {
+  if [ ! -e "${HOME}/.rustup" ] || [ ! -e "${HOME}/.cargo" ]; then
     curl -sL https://sh.rustup.rs | bash -s -- --no-modify-path -y
-  fi
-  rustup update
-  rustup component add rls rust-analysis rust-src
-}
-
-install_neovim() {
-  if ! command_exists nvim; then
-    brew install neovim
-    python3 -m pip install --user -U pynvim
+  else
+    rustup update
   fi
 }
 
-update_nvim_plugins() {
+setup_minpac() {
   # install minpac
   local minpac_dir="${HOME}/.config/nvim/pack/minpac/opt/minpac"
   if [ ! -e "$minpac_dir" ]; then
     mkdir -p "$minpac_dir"
     git clone "https://github.com/k-takata/minpac.git" "$minpac_dir"
   fi
-  # update plugins
-  # FIXME PackUpdate failing
-  # nvim --headless +PackUpdate +qall
-  # nvim --headless +CocUpdate +qall
 }
 
 setup_gpg_agent() {
   mkdir -p "${HOME}/.config/systemd/user"
-  pushd "${HOME}/.config/systemd/user"
+  pushd "${HOME}/.config/systemd/user" >/dev/null
   cp -f /usr/share/doc/gnupg/examples/systemd-user/* .
   systemctl --user daemon-reload
   systemctl --user enable *.socket
-  popd
+  popd >/dev/null
 }
 
 readonly targets=${*:-$(guess_target)}
@@ -87,33 +81,43 @@ mkdir -p "${HOME}/.config"
 
 for target in $targets; do
   case $target in
+    checkhealth)
+      # commands
+      check_command brew   || true
+      check_command cargo  || true
+      check_command clangd || true # coc-clangd
+      check_command ctags  || true # coc-fzf (uses for outline)
+      check_command go     || true
+      check_command gopls  || true # coc-go
+      check_command fzf    || true
+      check_command npm    || true
+      check_command nvim   || true
+      check_command pylint || true # coc-python (linter, uses mpls as server)
+      check_command rg     || true
+      check_command rls    || true # coc-rls
+      check_command rustup || true
+      check_command texlab || true # coc-texlab
+      check_command tmux   || true
+      ;;
     cli)
       stow cli
-      # install homebrew
-      install_brew
-      # source environment, may have changed
-      source "${HOME}/.profile"
-      # install programs
-      command_exists ctags || brew install ctags
-      command_exists npm   || brew install npm
-      command_exists go    || brew install go
-      install_rust
-      command_exists rg || cargo install ripgrep
-      go get -u golang.org/x/tools/gopls
-      python3 -m pip install --user -U jedi pylint rope
-      install_neovim
-      # update neovim stuff
-      update_nvim_plugins
+      setup_minpac
       ;;
     gui)
       stow gui
       setup_gpg_agent
       ;;
+    install_brew)
+      install_brew
+      ;;
+    install_rustup)
+      install_rustup
+      ;;
     work)
       stow work
       ;;
     *)
-      echo "Unknown target $target" 2>&1
+      (>&2 echo "Unknown target $target")
       exit 1
       ;;
   esac
