1835408cc078e7913290e869995437a1edf9d871
Robert Kernan <rkernan@gmail.com>
2020-05-19 14:13:36 -0400

    Use default fzf command in fzf-edit

--- cli/.config/fish/functions/fzf_edit.fish
+++ cli/.config/fish/functions/fzf_edit.fish
@@ -4,7 +4,7 @@ function fzf_edit --description "Fuzzy edit file(s)"
         set preview "head -"(__fzf_preview_height)" {}"
     end
 
-    set -l destfiles (rg --hidden --files --no-ignore --follow . 2>/dev/null | fzf -m --preview="$preview")
+    set -l destfiles (fzf -m --preview="$preview")
     if test -z $destfiles
         return 1
     end
