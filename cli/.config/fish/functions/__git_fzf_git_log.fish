53d84855a1c5d7e44891523ab1b543ba03867eb5
Robert Kernan <rkernan@gmail.com>
2020-05-23 13:04:58 -0400

    Limit 'git lg' hash filter to the first one found
    The message format dictates that the first hash found is always the
    commit hash. Any additional results are junk, so throw them away.

--- cli/.config/fish/functions/__git_fzf_git_log.fish
+++ cli/.config/fish/functions/__git_fzf_git_log.fish
@@ -5,7 +5,7 @@ function __git_fzf_git_log --description "Fuzzy select git commits"
 
     set -l preview
     if __fzf_show_preview
-        set preview "git show (echo {} | grep -o '[a-f0-9]\{7,\}') | head -"(__fzf_preview_height)
+        set preview "git show (echo {} | grep -o '[a-f0-9]\{7,\}' | head -1) | head -"(__fzf_preview_height)
     end
 
     git log --color=always --graph --date=short --format="%C(auto)%cd %h%d %s %C(magenta)[%an]%Creset" | \
