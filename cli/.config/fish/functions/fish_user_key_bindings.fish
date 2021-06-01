655559dfbd43b22bb27711636b452ec456aa742d
Robert Kernan <rkernan@gmail.com>
2021-06-01 17:48:42 -0400

    Make sure fzf is fully setup before setting up key bindings

--- cli/.config/fish/functions/fish_user_key_bindings.fish
+++ cli/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     fish_hybrid_key_bindings
     if type -q fzf
+    and type -q  fzf_key_bindings
         fzf_key_bindings
         git_fzf_key_bindings
         hosts_fzf_key_bindings
