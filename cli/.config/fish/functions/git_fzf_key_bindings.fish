bcbf5f13137f15f255d715f82ed1b346536ec650
Robert Kernan <rkernan@gmail.com>
2021-06-16 16:04:33 -0400

    Fixed fzf git history keybinding (collided with unified movement)

--- cli/.config/fish/functions/git_fzf_key_bindings.fish
+++ cli/.config/fish/functions/git_fzf_key_bindings.fish
@@ -2,12 +2,12 @@ function git_fzf_key_bindings
     bind \cg\cf __git_fzf_git_status
     bind \cg\cb __git_fzf_git_branch
     bind \cg\ct __git_fzf_git_tag
-    bind \cg\ch __git_fzf_git_log
+    bind \cg\ce __git_fzf_git_log
     
     if bind -M insert >/dev/null 2>&1
         bind -M insert \cg\cf __git_fzf_git_status
         bind -M insert \cg\cb __git_fzf_git_branch
         bind -M insert \cg\ct __git_fzf_git_tag
-        bind -M insert \cg\ch __git_fzf_git_log
+        bind -M insert \cg\ce __git_fzf_git_log
     end
 end
