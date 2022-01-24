7017aa3b7753845a0f5f6194d5ec29de742e06ec
Robert Kernan <rkernan@gmail.com>
2022-01-23 22:02:40 -0500

    Fixed issue where fzf default key bindings weren't enabled

--- cli/.config/fish/functions/fish_user_key_bindings.fish
+++ cli/.config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,7 @@ function fish_user_key_bindings
     fzf_git_user_key_bindings
     fzf_hosts_user_key_bindings
 
-    if type -q fzf_user_key_bindings
+    if type -q fzf_key_bindings
         fzf_key_bindings
     end
 end
