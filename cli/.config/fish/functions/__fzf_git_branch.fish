4c1a5780e2ced9d94cce14a74ef6ddcfaf78cb21
Robert Kernan <rkernan@gmail.com>
2022-02-01 01:52:44 -0500

    Use custom git log for relevant fzf-git commands

--- cli/.config/fish/functions/__fzf_git_branch.fish
+++ cli/.config/fish/functions/__fzf_git_branch.fish
@@ -5,7 +5,7 @@ function __fzf_git_branch -d "Fuzzy select git branch"
 
     set -l fzf_query (__fzf_parse_commandline)[2]
     set -l fzf_git_branch_command 'git branch --format "%(refname:short)"'
-    set -l fzf_preview 'git log --color --all --graph --oneline {} 2> /dev/null'
+    set -l fzf_preview 'git log --color {} 2> /dev/null'
 
     eval $fzf_git_branch_command | eval (__fzfcmd_with_preview $fzf_preview)' -m --query="'$fzf_query'"' | 
         while read -l r
--- cli/.config/fish/functions/__fzf_git_log.fish
+++ cli/.config/fish/functions/__fzf_git_log.fish
@@ -3,7 +3,7 @@ function __fzf_git_log -d "Fuzzy select git commits"
         return
     end
 
-    set -l fzf_git_log_command 'git log --color --oneline'
+    set -l fzf_git_log_command 'git log --color'
     set -l fzf_preview 'git show --color {1}'
 
     eval $fzf_git_log_command | eval (__fzfcmd_with_preview $fzf_preview)' -m --ansi' | awk '{print $1}' |
