60896f653a1431f286618ab65da6bd8e24f1d1f5
Robert Kernan <rkernan@gmail.com>
2022-02-01 18:44:42 -0500

    Restore tmux window name after ssh exits

--- cli/.config/fish/functions/ssh.fish
+++ cli/.config/fish/functions/ssh.fish
@@ -1,7 +1,9 @@
 function ssh
 	if not test -z $TMUX
+		set -l orig (tmux display-message -p '#W')
 		tmux rename-window "$argv"
 		command ssh $argv
+		tmux rename-window "$orig"
 		tmux set-window-option automatic-rename "on"
 	else
 		command ssh $argv
