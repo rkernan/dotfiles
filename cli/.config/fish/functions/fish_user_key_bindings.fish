812eceaa49778d57d4cf83d46d870d1989ece2ab
Robert Kernan <rkernan@gmail.com>
2022-02-01 19:27:54 -0500

    Removed fzf hosts keybindings

--- cli/.config/fish/functions/fish_user_key_bindings.fish
+++ cli/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,6 @@
 function fish_user_key_bindings
     fish_hybrid_key_bindings
     fzf_git_user_key_bindings
-    fzf_hosts_user_key_bindings
 
     if type -q fzf_key_bindings
         fzf_key_bindings
