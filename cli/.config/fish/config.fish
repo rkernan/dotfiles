9d44cc97d8ce570fe8e05e2324b8f1e98f8f9987
Robert Kernan <rkernan@gmail.com>
2022-02-03 00:54:25 -0500

    Split TMUX autostart into fish function

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -6,18 +6,7 @@ fish_add_path /home/linuxbrew/.linuxbrew/sbin
 fish_add_path /home/linuxbrew/.linuxbrew/bin
 fish_add_path $HOME/.local/bin
 
-if set -q AUTOSTART_TMUX
-and test $AUTOSTART_TMUX -gt 0
-and type -q tmux
-and status is-interactive
-and test -z "$TMUX"
-  set --local session 0
-
-  if not tmux has-session -t $session
-    tmux new-session -s $session
-  end
-  exec tmux attach-session -t $session
-end
+_autostart_tmux
 
 set -x EDITOR nvim
 set -x PAGER less
