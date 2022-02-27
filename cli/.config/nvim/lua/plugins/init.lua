7af420707ee51056262598e291c51ecd6e5f5871
Robert Kernan <rkernan@gmail.com>
2022-02-27 18:15:49 -0500

    Removed packer auto-sync on save

--- cli/.config/nvim/lua/plugins/init.lua
+++ cli/.config/nvim/lua/plugins/init.lua
@@ -1,11 +1,3 @@
--- automatically run PackerCompile whenever plugins.lua is updated
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup END
-]])
-
 -- use a protectd call so we don't error out on first use
 local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
