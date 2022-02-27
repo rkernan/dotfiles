3ed2497e609bf0c9f56ff2d6daa544747db09239
Robert Kernan <rkernan@gmail.com>
2022-02-27 18:10:05 -0500

    Fixed formatting

--- cli/.config/nvim/init.lua
+++ cli/.config/nvim/init.lua
@@ -39,45 +39,45 @@ vim.api.nvim_set_keymap('n', '<Space>', '', {})
 vim.g.mapleader = ' '
 
 -- disable ex mode
-vim.api.nvim_set_keymap('n', 'Q', '<nop>', {['noremap'] = true})
+vim.api.nvim_set_keymap('n', 'Q', '<nop>', { noremap = true })
 
 -- yank to end of line
-vim.api.nvim_set_keymap('n', 'Y', 'y$', {['noremap'] = true})
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
 
 -- treat lines wraps as real lines
-vim.api.nvim_set_keymap('n', 'j', 'gj', {['noremap'] = true})
-vim.api.nvim_set_keymap('n', 'k', 'gk', {['noremap'] = true})
-vim.api.nvim_set_keymap('n', 'gj', 'j', {['noremap'] = true})
-vim.api.nvim_set_keymap('n', 'gk', 'k', {['noremap'] = true})
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
+vim.api.nvim_set_keymap('n', 'gj', 'j', { noremap = true })
+vim.api.nvim_set_keymap('n', 'gk', 'k', { noremap = true })
 
 -- keep highlight after changing indent
-vim.api.nvim_set_keymap('v', '>', '>gv', {['noremap'] = true})
-vim.api.nvim_set_keymap('v', '<', '<gv', {['noremap'] = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
 
 -- automatically jump to the end of the last paste
-vim.api.nvim_set_keymap('v', 'y', 'y`]', {['noremap'] = true})
-vim.api.nvim_set_keymap('v', 'p', 'p`]', {['noremap'] = true})
-vim.api.nvim_set_keymap('n', 'p', 'p`]', {['noremap'] = true})
+vim.api.nvim_set_keymap('v', 'y', 'y`]', { noremap = true })
+vim.api.nvim_set_keymap('v', 'p', 'p`]', { noremap = true })
+vim.api.nvim_set_keymap('n', 'p', 'p`]', { noremap = true })
 
 -- disable highlighting until the next search
-vim.api.nvim_set_keymap('n', 'noh', ':noh<CR>', {['noremap'] = true, ['nowait'] = true, ['silent'] = true})
+vim.api.nvim_set_keymap('n', 'noh', ':noh<CR>', { noremap = true, nowait = true, silent = true })
 
 -- resize windows automatically
 vim.cmd([[
-	augroup vimrc_resize_windows
-		autocmd!
-		autocmd VimResized * wincmd =
-	augroup END
+augroup vimrc_resize_windows
+  autocmd!
+  autocmd VimResized * wincmd =
+augroup END
 ]])
 
 -- auto-switch to/from relative line numbers based on mode and window focus
 -- FIXME doesn't work with fzf
 vim.cmd([[
-	augroup vimrc_switch_line_numbers
-		autocmd!
-		autocmd InsertLeave * setlocal relativenumber
-		autocmd InsertEnter * setlocal norelativenumber
-		autocmd BufEnter,FocusGained * setlocal relativenumber cursorline
-		autocmd BufLeave,FocusLost * setlocal norelativenumber nocursorline
-	augroup END
+augroup vimrc_switch_line_numbers
+  autocmd!
+  autocmd InsertLeave * setlocal relativenumber
+  autocmd InsertEnter * setlocal norelativenumber
+  autocmd BufEnter,FocusGained * setlocal relativenumber cursorline
+  autocmd BufLeave,FocusLost * setlocal norelativenumber nocursorline
+augroup END
 ]])
