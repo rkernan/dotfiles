ed265304a34b5e418446cefefaf102bf4f9f6dc7
Robert Kernan <rkernan@gmail.com>
2022-03-01 04:23:14 -0500

    Don't use quickfix for mutliple fzf files

--- cli/.config/nvim/lua/plugins/fzf.lua
+++ cli/.config/nvim/lua/plugins/fzf.lua
@@ -1,3 +1,12 @@
+local actions = require('fzf-lua.actions')
+require('fzf-lua').setup({
+  files = {
+    actions = {
+      ['default'] = actions.file_edit
+    }
+  }
+})
+
 local opts = { noremap = true }
 vim.api.nvim_set_keymap('n', '<leader>b', "<cmd>lua require('fzf-lua').buffers()<CR>", opts)
 vim.api.nvim_set_keymap('n', '<leader>f', "<cmd>lua require('fzf-lua').files()<CR>", opts)
