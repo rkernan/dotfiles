e70fb845306c14a0245e7038e12f95aabd3eed63
Robert Kernan <rkernan@gmail.com>
2022-03-01 01:51:17 -0500

    Moved lsp fzf mappings to plugins.lspconfig

--- cli/.config/nvim/lua/plugins/fzf.lua
+++ cli/.config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,4 @@
-vim.api.nvim_set_keymap('n', '<leader>b', [[ <cmd>lua require('fzf-lua').buffers()<CR> ]],   { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>f', [[ <cmd>lua require('fzf-lua').files()<CR> ]],     { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>/', [[ <cmd>lua require('fzf-lua').live_grep()<CR> ]], { noremap = true })
-
--- LSP
-vim.api.nvim_set_keymap('n', '<leader><leader>a', [[ <cmd>lua require('fzf-lua').lsp_code_actions()<CR> ]],         { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader><leader>b', [[ <cmd>lua require('fzf-lua').lsp_document_diagnostics()<CR> ]], { noremap = true })
+local opts = { noremap = true }
+vim.api.nvim_set_keymap('n', '<leader>b', "<cmd>lua require('fzf-lua').buffers()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<leader>f', "<cmd>lua require('fzf-lua').files()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<leader>/', "<cmd>lua require('fzf-lua').live_grep()<CR>", opts)
