30ec71e43c13fdbbb567f56b97426ae2a586f575
Robert Kernan <rkernan@gmail.com>
2022-03-02 13:00:40 -0500

    Replaced manual virtualenv setup with virtualfish

--- cli/.config/fish/functions/fish_prompt.fish
+++ cli/.config/fish/functions/fish_prompt.fish
@@ -3,18 +3,18 @@ function fish_prompt
 
   set -l num_jobs (jobs | wc -l | tr -d '[:space:]')
   if test $num_jobs -gt 0
-    echo -n (set_color --bold yellow)$num_jobs" "(set_color normal)
+    echo -n -s (set_color --bold yellow) "$num_jobs" (set_color normal) " "
   end
 
-  echo -n (__fish_print_pipestatus "" " " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)(set_color normal)
+  echo -n -s (__fish_print_pipestatus "" " " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color blue white) "(" (basename $VIRTUAL_ENV) ")" (set_color normal) " "
+  end
 
   if set -q SSH_CLIENT || set -q SSH_TTY
-    echo -n (set_color $fish_color_host_remote)(prompt_hostname)(set_color normal)":"(set_color normal)
+    echo -n -s (set_color $fish_color_host_remote) (prompt_hostname) (set_color normal) ":"
   end
-
-  echo -n (set_color $fish_color_cwd)(prompt_pwd)(set_color normal)
-
-  echo -n (set_color normal)(fish_vcs_prompt)(set_color normal)
 
   set -l prompt_suffix
   switch $fish_bind_mode
@@ -30,5 +30,5 @@ function fish_prompt
       set prompt_suffix $__fish_vi_prompt_visual_suffix
   end
 
-  echo -n (set_color normal)" $prompt_suffix "(set_color normal)
+  echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (fish_vcs_prompt) (set_color normal) " $prompt_suffix "
 end
