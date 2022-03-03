4b30c080034cfdcb270f3e139877052727fe848e
Robert Kernan <rkernan@gmail.com>
2022-03-03 00:48:05 -0500

    Cleaned up fish config

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -1,3 +1,12 @@
+fish_add_path $HOME"/.local/bin"
+
+# asdf
+set -l asdf_path $HOME"/.asdf"
+if test -d $asdf_path
+  source $asdf_path"/asdf.fish"
+end
+
+# brew
 set -l brew_prefix /home/linuxbrew/.linux
 if test -d $brew_prefix
   # path
@@ -8,15 +17,24 @@ if test -d $brew_prefix
   set -gx fish_complete_path $fish_complete_path $brew_prefix"/share/fish/vendor_completions.d"
 end
 
-set -x GOPATH ~/Workspace/go
-fish_add_path $GOPATH"/bin"
-
-fish_add_path $HOME"/.local/bin"
+# go
+set -l go_path $HOME"/workspace/go"
+if test -d $go_path
+  set -x GOPATH $go_path
+  fish_add_path $GOPATH"/bin"
+end
 
 _autostart_tmux
 
 set -q NO_UNICODE || set -x NO_UNICODE 0
 
+# command colors
+set fish_color_command normal
+set fish_color_redirection brblack --bold
+set fish_color_end brblack --bold
+set fish_color_param normal
+
+# prompt colors and symbols
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_color_branch magenta --bold
 set -g __fish_git_prompt_showupstream "informative"
@@ -33,7 +51,7 @@ set -g __fish_vi_prompt_replace_one_suffix "<"
 set -g __fish_vi_prompt_replace_suffix "<"
 set -g __fish_vi_prompt_visual_suffix "<"
 
-if set -q NO_UNICODE; and test $NO_UNICODE -gt 0
+if test $NO_UNICODE -gt 0
   set -g __fish_git_prompt_char_upstream_ahead "↑"
   set -g __fish_git_prompt_char_upstream_behind "↓"
   set -g __fish_git_prompt_char_cleanstate "!"
@@ -51,8 +69,8 @@ else
   set -g __fish_git_prompt_char_untrackedfiles "…"
 end
 
+# fzf config
 set -x FZF_DEFAULT_OPTS "--cycle --layout=reverse --border --height=75% --info=inline"
-
 set -x FZF_DEFAULT_COMMAND "_fzf_list_files \$dir"
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -x FZF_CTRL_T_OPTS "--preview='_fzf_file_preview {}'"
@@ -60,9 +78,11 @@ set -x FZF_CTRL_R_OPTS "$FZF_DEFAULT_OPTS --prompt='History> '"
 set -x FZF_ALT_C_COMMAND "_fzf_list_dirs \$dir"
 set -x FZF_ALT_C_OPTS "$FZF_DEFAULT_COMMAND --prompt='Dirs> '"
 
+# env
 set -x EDITOR nvim
 set -x PAGER less
 
+# aliases
 abbr e $EDITOR
 abbr p $PAGER
 
@@ -70,11 +90,6 @@ abbr cp cp -i
 abbr ln ln -i
 abbr mv mv -i
 abbr rm rm -i
-
-set fish_color_command normal
-set fish_color_redirection brblack --bold
-set fish_color_end brblack --bold
-set fish_color_param normal
 
 # sane umask
 umask 0022
