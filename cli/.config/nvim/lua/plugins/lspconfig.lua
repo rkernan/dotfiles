4b5b1d2d0573c474a95e9168102325c029633f89
Robert Kernan <rkernan@gmail.com>
2022-03-03 01:59:59 -0500

    Use <leader><leader> for all LSP mappings

--- cli/.config/nvim/lua/plugins/lspconfig.lua
+++ cli/.config/nvim/lua/plugins/lspconfig.lua
@@ -6,29 +6,26 @@ local opts = { noremap = true }
 
 -- setup buffer when language server starts
 local on_attach = function(client, bufnr)
-  vim.api.nvim_set_keymap('n', '<space>e', "<cmd>lua require('fzf-lua').lsp_document_diagnostics()<CR>", opts)
+  -- code actions
+  vim.api.nvim_set_keymap('n', '<leader><leader>a', "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_set_keymap('v', '<leader><leader>a', "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
+  -- diagnostics - buffer
+  vim.api.nvim_set_keymap('n', '<space>d', "<cmd>lua require('fzf-lua').lsp_document_diagnostics()<CR>", opts)
   vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostics.goto_prev()<CR>', opts)
   vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostics.goto_next()<CR>', opts)
-
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', "<cmd>lua require('fzf-lua').lsp_declarations()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', "<cmd>lua require('fzf-lua').lsp_definitions()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', "<cmd>lua require('fzf-lua').lsp_implementations()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', "<cmd>lua require('fzf-lua').lsp_typedefs()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', "<cmd>lua require('fzf-lua').lsp_code_actions()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', "<cmd>lua require('fzf-lua').lsp_references()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  -- format
+  vim.api.nvim_set_keymap('n', '<leader><leader>a', "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  vim.api.nvim_set_keymap('v', '<leader><leader>a', "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  -- goto
+  vim.api.nvim_set_keymap('n', '<leader><leader>r', "<cmd>lua require('fzf-lua').lsp_references()<CR>", opts)
+  vim.api.nvim_set_keymap('n', '<leader><leader>i', "<cmd>lua require('fzf-lua').lsp_implementations()<CR>", opts)
+  vim.api.nvim_set_keymap('n', '<leader><leader>d', "<cmd>lua require('fzf-lua').lsp_definitions()<CR>", opts)
+  vim.api.nvim_set_keymap('n', '<leader><leader>D', "<cmd>lua require('fzf-lua').lsp_declarations()<CR>", opts)
+  vim.api.nvim_set_keymap('n', '<leader><leader>t', "<cmd>lua require('fzf-lua').lsp_typedefs()<CR>", opts)
+  -- hover doc
+  vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  -- symbols
+  vim.api.nvim_set_keymap('n', '<leader><leader>s', "<cmd>lua require('fzf-lua').lsp_document_symbols()<CR>", opts)
 end
 
 -- enable language servers
