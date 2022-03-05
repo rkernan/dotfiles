5f3e7c369f3aecd147d13cd15c4e8c490bd7df82
Robert Kernan <rkernan@gmail.com>
2022-03-05 14:37:01 -0500

    Customized lualine to look more like modestatus

--- cli/.config/nvim/lua/plugins/lualine.lua
+++ cli/.config/nvim/lua/plugins/lualine.lua
@@ -11,5 +11,29 @@ require('lualine').setup({
     icons_enabled = false,
     component_separators = { left = '', right = '|'},
     section_separators = { left = '', right = ''}
-  }
+  },
+  sections = {
+    lualine_a = {{
+      'mode',
+      fmt = function(str) return str:sub(1, 1) end
+    }},
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {{
+      'filename',
+      path = 1 -- relative path
+    }},
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'location', 'progress' },
+    lualine_z = {}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lauline_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
 })
