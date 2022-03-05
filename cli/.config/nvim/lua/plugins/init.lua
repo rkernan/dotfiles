4e1a887fee61d3d74ba1c784494d8f53bd164c07
Robert Kernan <rkernan@gmail.com>
2022-03-05 14:49:58 -0500

    Removed unused cmp-path plugin

--- cli/.config/nvim/lua/plugins/init.lua
+++ cli/.config/nvim/lua/plugins/init.lua
@@ -68,7 +68,6 @@ return packer.startup(function(use)
   })
   use('hrsh7th/cmp-nvim-lsp')
   use('hrsh7th/cmp-buffer')
-  use('hrsh7th/cmp-path')
   use('hrsh7th/cmp-cmdline')
   use({
     'hrsh7th/nvim-cmp',
