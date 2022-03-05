f93c7e9f629c80de5b6d8617044c9b81cf0f4eb7
Robert Kernan <rkernan@gmail.com>
2022-03-05 16:29:27 -0500

    Removed lualine right separator

--- cli/.config/nvim/lua/plugins/lualine.lua
+++ cli/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ require('lualine').setup({
   options = {
     theme = custom_gruvbox,
     icons_enabled = false,
-    component_separators = { left = '', right = '|'},
+    component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''}
   },
   sections = {
