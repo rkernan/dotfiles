7642a73ebbfc06029acbffc8528d8adbe98a0e19
Robert Kernan <rkernan@gmail.com>
2022-03-05 14:33:40 -0500

    Don't change lualine section_c colors on mode

--- cli/.config/nvim/lua/plugins/lualine.lua
+++ cli/.config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,13 @@
+local custom_gruvbox = require('lualine.themes.gruvbox')
+-- don't change c-section colors on mode change
+custom_gruvbox.insert.c = custom_gruvbox.normal.c
+custom_gruvbox.visual.c = custom_gruvbox.normal.c
+custom_gruvbox.replace.c = custom_gruvbox.normal.c
+custom_gruvbox.command.c = custom_gruvbox.normal.c
+
 require('lualine').setup({
   options = {
+    theme = custom_gruvbox,
     icons_enabled = false,
     component_separators = { left = '', right = '|'},
     section_separators = { left = '', right = ''}
