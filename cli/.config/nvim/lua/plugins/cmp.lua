ab3acbfb15afc6f4dbe5071986ee8db762e566f4
Robert Kernan <rkernan@gmail.com>
2022-03-07 23:17:37 -0500

    Removed unused cmp mappings

--- cli/.config/nvim/lua/plugins/cmp.lua
+++ cli/.config/nvim/lua/plugins/cmp.lua
@@ -4,8 +4,6 @@ cmp.setup({
   mapping = {
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4),  { 'i', 'c' }),
-    ['<C-Space'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable,
     ['<C-e'] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close()
