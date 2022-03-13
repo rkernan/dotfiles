d69ae79c70f58db9fe28c491ab7abdee650e2130
Robert Kernan <rkernan@gmail.com>
2022-03-13 14:34:02 -0400

    Fixed unicode git prompt display

--- cli/.config/fish/conf.d/prompt.fish
+++ cli/.config/fish/conf.d/prompt.fish
@@ -6,8 +6,8 @@ set fish_color_redirection brblack --bold
 set fish_color_end brblack --bold
 set fish_color_param normal
 
-# prompt colors and symbols
-set -g __fish_git_prompt_show_informative_status 1
+# git prompt colors and symbols
+set -g __fish_git_prompt_show_informative_status true
 set -g __fish_git_prompt_color_branch magenta --bold
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_prefix ""
@@ -20,18 +20,17 @@ set -g __fish_git_prompt_color_untrackedfiles red
 if set -q NO_UNICODE || test -z "$NO_UNICODE"
   set -g __fish_git_prompt_char_upstream_ahead "↑"
   set -g __fish_git_prompt_char_upstream_behind "↓"
+  set -g __fish_git_prompt_char_cleanstate "✓"
+  set -g __fish_git_prompt_char_stagedstate "•"
+  set -g __fish_git_prompt_char_dirtystate "+"
+  set -g __fish_git_prompt_char_invalidstate "⨯"
+  set -g __fish_git_prompt_char_untrackedfiles "…"
+else
+  set -g __fish_git_prompt_char_upstream_ahead "A"
+  set -g __fish_git_prompt_char_upstream_behind "B"
   set -g __fish_git_prompt_char_cleanstate "!"
   set -g __fish_git_prompt_char_stagedstate "+"
   set -g __fish_git_prompt_char_dirtystate "+"
   set -g __fish_git_prompt_char_invalidstate "x"
   set -g __fish_git_prompt_char_untrackedfiles "??"
-else
-  set -g __fish_git_prompt_char_upstream_ahead "↑"
-  set -g __fish_git_prompt_char_upstream_behind "↓"
-  set -g __fish_git_prompt_char_cleanstate "✓"
-  set -g __fish_git_prompt_char_stagedstate "·"
-  set -g __fish_git_prompt_char_dirtystate "+"
-  set -g __fish_git_prompt_char_invalidstate "⨯"
-  set -g __fish_git_prompt_char_untrackedfiles "…"
 end
-
