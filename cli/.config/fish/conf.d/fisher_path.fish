f7c69f6bdba849d7c6c1be60d139db5f58691d5b
Robert Kernan <rkernan@gmail.com>
2022-03-13 14:21:15 -0400

    Only install fisher in interactive shell

--- cli/.config/fish/conf.d/fisher_path.fish
+++ cli/.config/fish/conf.d/fisher_path.fish
@@ -5,9 +5,13 @@ if test -z "$fisher_path" || test "$fisher_path" = "$__fish_config_dir"
     exit
 end
 
-if not test -d "$fisher_path"
+if status is-interactive && not test -f $fisher_path"/functions/fisher.fish"
     # automatically install fisher
     curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    # FIXME https://github.com/jorgebucaran/fisher/issues/705
+    echo "Fisher install clobbers fish_plugins... Run the following:"
+    echo "> git checkout fish_plugins"
+    echo "> cat fish_plugins | fisher install"
 end
 
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
