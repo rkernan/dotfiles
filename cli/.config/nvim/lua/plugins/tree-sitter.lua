8a66ae1073336559fd0d6d54ee2096e000ffba67
Robert Kernan <rkernan@gmail.com>
2022-03-20 13:22:04 -0400

    Removed treesitter endwise and context-commentstring

--- cli/.config/nvim/lua/plugins/tree-sitter.lua
+++ cli/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,6 +1,5 @@
 require('nvim-treesitter.configs').setup({
   ensure_installed = 'maintained',
   context_commentstring = { enable = true },
-  endwise = { enable = true },
   highlight = { enable = true }
 })
