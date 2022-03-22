070e71f07df83797eddb58f8167827db26821336
Robert Kernan <rkernan@gmail.com>
2022-03-22 16:14:54 -0400

    Categorize plugins based on nvim features

--- cli/.config/nvim/lua/plugins/init.lua
+++ cli/.config/nvim/lua/plugins/init.lua
@@ -17,35 +17,29 @@ packer.init({
 -- install plugins
 return packer.startup(function(use)
   -- let packer manage itself
-  use('wbthomason/packer.nvim')
-  -- appearance
-  use({ 'gruvbox-community/gruvbox', config = require('plugins.gruvbox').setup })
-  use({ 'nvim-lualine/lualine.nvim', config = function() require('plugins.lualine') end })
-  -- help popup for mappings, etc
+  use({ 'wbthomason/packer.nvim' })
+  -- general
   use({ "folke/which-key.nvim", config = function() require("which-key").setup({}) end })
-  -- text objects
-  use('wellle/targets.vim')
-  -- vcs integration
-  use({ 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' }, config = function() require('plugins.gitsigns') end })
-  -- fzf integration
+  use({ 'gruvbox-community/gruvbox', config = require('plugins.gruvbox').setup })
   use({ 'ibhagwan/fzf-lua', config = function() require('plugins.fzf') end })
-  -- other
   use({ 'lambdalisue/suda.vim', setup = function() vim.g.suda_smart_edit = 1 end })
-  use('tpope/vim-repeat')
-  use('tpope/vim-surround')
-  use('tpope/vim-sleuth')
-  use('tpope/vim-unimpaired')
-  -- completion
+  use({ 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' }, config = function() require('plugins.gitsigns') end })
+  use({ 'nvim-lualine/lualine.nvim', config = function() require('plugins.lualine') end })
+  use({ 'tpope/vim-commentary' })
+  use({ 'tpope/vim-repeat' })
+  use({ 'tpope/vim-surround' })
+  use({ 'tpope/vim-sleuth' })
+  use({ 'tpope/vim-unimpaired' })
+  use({ 'windwp/nvim-autopairs', config = function() require('plugins.autopairs') end })
+  use({ 'wellle/targets.vim' })
+  -- lspconfig
   use({ 'neovim/nvim-lspconfig', config = function() require('plugins.lspconfig') end })
-  use('hrsh7th/cmp-nvim-lsp')
-  use('hrsh7th/cmp-buffer')
-  use('hrsh7th/cmp-cmdline')
+  use({ 'hrsh7th/cmp-nvim-lsp' })
+  use({ 'hrsh7th/cmp-buffer' })
+  use({ 'hrsh7th/cmp-cmdline' })
   use({ 'hrsh7th/nvim-cmp', config = function() require('plugins.cmp') end })
-  -- snippets - required
-  use('L3MON4D3/LuaSnip')
-  use('saadparwaiz1/cmp_luasnip')
-  -- languages
-  use({ 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup({}) end })
-  use('tpope/vim-commentary')
+  use({ 'L3MON4D3/LuaSnip' })
+  use({ 'saadparwaiz1/cmp_luasnip' })
+  -- treesitter - requires nvim 0.6, experimental
   use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function() require('plugins.tree-sitter') end })
 end)
