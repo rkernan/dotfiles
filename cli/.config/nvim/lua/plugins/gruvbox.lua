a0f9e67b03dbb095f719f52403fb9b4f5da17da1
Robert Kernan <rkernan@gmail.com>
2022-10-07 14:25:52 -0400

    Italics for most keywords, excluding operators

--- cli/.config/nvim/lua/plugins/gruvbox.lua
+++ cli/.config/nvim/lua/plugins/gruvbox.lua
@@ -2,7 +2,12 @@ local palette = require('gruvbox.palette')
 
 require('gruvbox').setup({
   overrides = {
+    Conditional = { fg = palette.bright_red, italic = true },
+    Repeat = { fg = palette.bright_red, italic = true },
+    Label = { fg = palette.bright_red, italic = true },
+    Operator = { fg = palette.bright_orange, italic = false },
     Keyword = { fg = palette.bright_red, italic = true },
+    Exception = { fg = palette.bright_red, italic = true },
     String = { italic = false },
     LspSignatureActiveParameter = { fg = palette.bright_aqua, bold = true}
   }
