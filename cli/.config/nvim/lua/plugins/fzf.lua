9bc0ea11fd470dcd8302ba0c99a536c3231b6090
Robert Kernan <rkernan@gmail.com>
2022-10-09 15:59:05 -0400

    Disable devicons files icons in fzf

--- cli/.config/nvim/lua/plugins/fzf.lua
+++ cli/.config/nvim/lua/plugins/fzf.lua
@@ -9,18 +9,31 @@ fzf.setup({
     }
   },
   files = {
+    file_icons = false,
     actions = {
       ['default'] = require('fzf-lua.actions').file_edit
     }
   },
+  git = {
+    files = { file_icons = false },
+    status = { file_icons = false },
+  },
+  grep = { file_icons = false },
+  buffers = { file_icons = false },
+  tabs = { file_icons = false },
+  tags = { file_icons = false },
+  btags = { file_icons = false },
+  quickfix = { file_icons = false },
   lsp = {
+    file_icons = false,
     icons = {
       ["Error"] = { icon = "E", color = "red" },
       ["Warning"] = { icon = "W", color = "yellow" },
       ["Information"] = { icon = "I", color = "blue" },
       ["Hint"] = { icon = "H", color = "magenta" }
     },
-  }
+  },
+  diagnostics = { file_icons = false },
 })
 
 vim.keymap.set('n', '<leader>b', fzf.buffers)
