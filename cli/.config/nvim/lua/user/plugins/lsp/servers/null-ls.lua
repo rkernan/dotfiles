e9a9e7edee96fe2410cc6dd600069136f7aef23d
Robert Kernan <rkernan@gmail.com>
2022-10-16 20:58:38 +0000

    Pycode style severity to WARN, pydocstyle severity to INFO

--- cli/.config/nvim/lua/user/plugins/lsp/servers/null-ls.lua
+++ cli/.config/nvim/lua/user/plugins/lsp/servers/null-ls.lua
@@ -8,8 +8,17 @@ M.setup = function (on_attach, capabilities)
       -- python
       -- null_ls.builtins.formatting.black,
       null_ls.builtins.formatting.isort,
-      null_ls.builtins.diagnostics.pycodestyle.with({ extra_args = { '--max-line-length=119' }}),
-      null_ls.builtins.diagnostics.pydocstyle,
+      null_ls.builtins.diagnostics.pycodestyle.with({
+        extra_args = { '--max-line-length=119' },
+        diagnostics_postprocess = function (diagnostic)
+          diagnostic.severity = vim.diagnostic.severity['WARN']
+        end
+      }),
+      null_ls.builtins.diagnostics.pydocstyle.with({
+        diagnostics_postprocess = function (diagnostic)
+          diagnostic.severity = vim.diagnostic.severity['INFO']
+        end
+      }),
       -- fish
       null_ls.builtins.diagnostics.fish,
     }
