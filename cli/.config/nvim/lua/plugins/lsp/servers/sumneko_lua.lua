332bea3cb19dc3337fdfd41d8cd2497f4384efa3
Robert Kernan <rkernan@gmail.com>
2022-11-22 23:41:31 +0000

    Disable sumneko prompt on startup

--- cli/.config/nvim/lua/plugins/lsp/servers/sumneko_lua.lua
+++ cli/.config/nvim/lua/plugins/lsp/servers/sumneko_lua.lua
@@ -3,7 +3,19 @@ local M = {}
 function M.setup(capabilities)
   require('lspconfig').sumneko_lua.setup({
     capabilities = capabilities,
-    settings = {Lua = {telemetry = { enabled = false }}}
+    settings = {
+      Lua = {
+        telemetry = {
+          enabled = false,
+        },
+        workspace = {
+          checkThirdParty = false,
+        },
+        completion = {
+          callSnippet = 'Replace',
+        }
+      }
+    }
   })
 end
 
