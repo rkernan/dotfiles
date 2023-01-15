f2393441470eed382cf3c27a5bcab7dfbc376f9f
Robert Kernan <rkernan@gmail.com>
2023-01-15 19:31:58 +0000

    Added mini.trailspace

--- cli/.config/nvim/lua/user/plugins/mini.lua
+++ cli/.config/nvim/lua/user/plugins/mini.lua
@@ -21,5 +21,6 @@ require('mini.ai').setup({
 })
 
 require('mini.align').setup()
-require('mini.comment').setup({})
-require('mini.surround').setup({})
+require('mini.comment').setup()
+require('mini.surround').setup()
+require('mini.trailspace').setup()
