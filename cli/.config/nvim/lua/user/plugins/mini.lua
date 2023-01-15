fc64c02a0f488acb8c18429206e1d33044ca16cd
Robert Kernan <rkernan@gmail.com>
2023-01-15 19:36:35 +0000

    Enabled mini.move

--- cli/.config/nvim/lua/user/plugins/mini.lua
+++ cli/.config/nvim/lua/user/plugins/mini.lua
@@ -22,5 +22,6 @@ require('mini.ai').setup({
 
 require('mini.align').setup()
 require('mini.comment').setup()
+require('mini.move').setup()
 require('mini.surround').setup()
 require('mini.trailspace').setup()
