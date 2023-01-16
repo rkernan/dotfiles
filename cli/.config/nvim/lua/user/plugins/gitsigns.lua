07600c019b25feb6ba39d6d3a2884b7f0f114f8b
Robert Kernan <rkernan@gmail.com>
2023-01-16 21:44:17 +0000

    Wrap all plugins mappings in function to avoid error on new setup

--- cli/.config/nvim/lua/user/plugins/gitsigns.lua
+++ cli/.config/nvim/lua/user/plugins/gitsigns.lua
@@ -6,18 +6,18 @@ return {
   event = { 'BufNew', 'BufRead' },
   keys = {
     -- navigation
-    { '[h', require('gitsigns').prev_hunk, desc = 'Git next hunk' },
-    { ']h', require('gitsigns').next_hunk, desc = 'Git prev hunk' },
+    { '[h', function () require('gitsigns').prev_hunk() end, desc = 'Git next hunk' },
+    { ']h', function () require('gitsigns').next_hunk() end, desc = 'Git prev hunk' },
     -- actions
-    { '<leader>hs', require('gitsigns').stage_hunk, mode = { 'n', 'x' }, desc = 'Git stage hunk' },
-    { '<leader>hr', require('gitsigns').reset_hunk, mode = { 'n', 'x'}, desc = 'Git reset hunk' },
-    { '<leader>hS', require('gitsigns').stage_buffer, desc = 'Git stage buffer' },
-    { '<leader>hu', require('gitsigns').undo_stage_hunk, desc = 'Git unstage buffer' },
-    { '<leader>hR', require('gitsigns').reset_buffer, desc = 'Git reset buffer' },
-    { '<leader>hp', require('gitsigns').preview_hunk, desc = 'Git preview hunk' },
+    { '<leader>hs', function () require('gitsigns').stage_hunk() end, mode = { 'n', 'x' }, desc = 'Git stage hunk' },
+    { '<leader>hr', function () require('gitsigns').reset_hunk() end, mode = { 'n', 'x'}, desc = 'Git reset hunk' },
+    { '<leader>hS', function () require('gitsigns').stage_buffer() end, desc = 'Git stage buffer' },
+    { '<leader>hu', function () require('gitsigns').undo_stage_hunk() end, desc = 'Git unstage buffer' },
+    { '<leader>hR', function () require('gitsigns').reset_buffer() end, desc = 'Git reset buffer' },
+    { '<leader>hp', function () require('gitsigns').preview_hunk() end, desc = 'Git preview hunk' },
     { '<leader>hb', function () require('gitsigns').blame_line({ full = true }) end, desc = 'Git blame line' },
-    { '<leader>ht', require('gitsigns').toggle_current_line_blame, desc = 'Git toggle blame' },
-    { '<leader>hd', require('gitsigns').diffthis, desc = 'Git diff base' },
+    { '<leader>ht', function () require('gitsigns').toggle_current_line_blame() end, desc = 'Git toggle blame' },
+    { '<leader>hd', function () require('gitsigns').diffthis() end, desc = 'Git diff base' },
     { '<leader>hD', function () require('gitsigns').diffthis('~') end, desc = 'Git diff prev commit' },
     -- text object
     { 'ih', ':<C-u>Gitsigns select_hunk<cr>', mode = { 'o', 'x' }, desc = 'Git select hunk' },
