c0cc54060cdabce6abe634a80f72f3ab8131e84f
Robert Kernan <rkernan@gmail.com>
2023-01-22 18:03:44 +0000

    Load everything in user.plugins instead of specifying everything

--- cli/.config/nvim/lua/user/lazy.lua
+++ cli/.config/nvim/lua/user/lazy.lua
@@ -11,28 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  -- appearance
-  require('user.plugins.feline'),
-  require('user.plugins.noice'),
-  require('user.plugins.telescope'),
-  require('user.plugins.which-key'),
-  require('user.plugins.zenbones'),
-  -- utils
-  require('user.plugins.asterisk'),
-  require('user.plugins.autopairs'),
-  require('user.plugins.gitsigns'),
-  require('user.plugins.mini'),
-  require('user.plugins.sleuth'),
-  require('user.plugins.suda'),
-  require('user.plugins.web-devicons'),
-  -- lsp
-  require('user.plugins.lspconfig'),
-  require('user.plugins.null-ls'),
-  -- completion
-  require('user.plugins.cmp'),
-  -- treesitter
-  require('user.plugins.treesitter'),
-  -- refactoring
-  require('user.plugins.refactoring'),
-})
+require('lazy').setup('user.plugins')
