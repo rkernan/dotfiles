6c0e725bf399bca3da116f8e1880f63be7d6439d
Robert Kernan <rkernan@gmail.com>
2023-03-05 21:12:02 +0000

    Added back vim-asterisk is hlslens dep

--- cli/.config/nvim/lua/user/plugins/hlslens.lua
+++ cli/.config/nvim/lua/user/plugins/hlslens.lua
@@ -1,14 +1,17 @@
 return {
   'kevinhwang91/nvim-hlslens',
+  dependencies = {
+    'haya14busa/vim-asterisk',
+  },
   keys = {
     { '/', [[/]], mode = { 'n', 'o' }, desc = 'Search' },
     { '?', [[?]], mode = { 'n', 'o' }, desc = 'Search backwards' },
     { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'o' }, desc = 'Next search result' },
     { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'o' }, desc = 'Previous search result' },
-    { '*', [[*<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search word' },
-    { '#', [[#<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search word backwards' },
-    { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search sub-word' },
-    { 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search sub-word backwards' },
+    { '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search word' },
+    { '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search word backwards' },
+    { 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search sub-word' },
+    { 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x', 'o' }, desc = 'Search sub-word backwards' },
   },
   opts = {
     calm_down = true,
