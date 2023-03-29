6ca180e155b2de8daa37f30c1198d46010d42c78
Robert Kernan <rkernan@gmail.com>
2023-03-29 16:50:56 +0000

    Removed autotag and endwise

--- cli/.config/nvim/lua/user/plugins/treesitter.lua
+++ cli/.config/nvim/lua/user/plugins/treesitter.lua
@@ -4,15 +4,11 @@ return {
   build = ':TSUpdate',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    'RRethy/nvim-treesitter-endwise',
-    'windwp/nvim-ts-autotag',
   },
   config = function ()
-    require('nvim-ts-autotag').setup()
     require('nvim-treesitter.configs').setup({
       ensure_installed = 'all',
       highlight = { enable = true },
-      endwise = { enable = true },
     })
   end,
 }
