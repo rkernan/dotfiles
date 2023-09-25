6b92884ed6e94d2767222ba919bcd1ace1b1e948
Robert Kernan <rkernan@gmail.com>
2023-09-25 04:24:33 +0000

    Added nvim-treesitter-endwise

--- cli/.config/nvim/lua/user/plugins/treesitter.lua
+++ cli/.config/nvim/lua/user/plugins/treesitter.lua
@@ -4,11 +4,13 @@ return {
   build = ':TSUpdate',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    'RRethy/nvim-treesitter-endwise',
   },
   config = function ()
     require('nvim-treesitter.configs').setup({
       ensure_installed = 'all',
       highlight = { enable = true },
+      endwise = { enable = true },
     })
   end,
 }
