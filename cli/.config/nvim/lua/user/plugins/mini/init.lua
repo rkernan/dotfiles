3a6b870b505224f79edb2825544e435db9c11477
Robert Kernan <rkernan@gmail.com>
2023-09-26 20:33:25 +0000

    Added mini.files plugin

--- cli/.config/nvim/lua/user/plugins/mini/init.lua
+++ cli/.config/nvim/lua/user/plugins/mini/init.lua
@@ -1,11 +1,15 @@
 return {
   'echasnovski/mini.nvim',
   lazy = false,
+  keys = {
+    { '<Leader>F', function () MiniFiles.open() end, desc = 'file explorer' },
+  },
   config = function ()
     require('mini.ai').setup()
     require('mini.align').setup()
     require('mini.bracketed').setup()
     require('mini.comment').setup()
+    require('mini.files').setup()
     require('mini.jump').setup()
     require('mini.jump2d').setup()
     require('mini.move').setup()
