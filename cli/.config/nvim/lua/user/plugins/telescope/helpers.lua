5b58a9c4c2052a64d1c25d5c99c3287dfc065160
Robert Kernan <rkernan@gmail.com>
2023-09-29 19:56:24 +0000

    Allow overriding some picker options when using helpers

--- cli/.config/nvim/lua/user/plugins/telescope/helpers.lua
+++ cli/.config/nvim/lua/user/plugins/telescope/helpers.lua
@@ -1,11 +1,13 @@
 local M = {}
 
-function M.local_diagnostics()
-  require('telescope.builtin').diagnostics({ layout_strategy = 'vertical', bufnr = 0 })
+function M.local_diagnostics(opts)
+  opts = vim.tbl_extend('force', opts or {}, { layout_strategy = 'vertical', bufnr = 0 })
+  require('telescope.builtin').diagnostics(opts)
 end
 
-function M.workspace_diagnostics()
-  require('telescope.builtin').diagnostics({ layout_strategy = 'vertical' })
+function M.workspace_diagnostics(opts)
+  opts = vim.tbl_extend('force', opts or {}, { layout_strategy = 'vertical' })
+  require('telescope.builtin').diagnostics(opts)
 end
 
 return M
