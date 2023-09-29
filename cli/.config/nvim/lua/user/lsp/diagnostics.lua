e52726be67ae45f095863f9c82b5f9e65226dce1
Robert Kernan <rkernan@gmail.com>
2023-09-29 20:13:04 +0000

    Simplified diagnostics - use virtual text

--- cli/.config/nvim/lua/user/lsp/diagnostics.lua
+++ cli/.config/nvim/lua/user/lsp/diagnostics.lua
@@ -1,48 +1,4 @@
-local lsp_utils = require('user.lsp.utils')
-local augroup = vim.api.nvim_create_augroup('lsp.diagnostics', { clear = true })
-
-local function open_float(args)
-  local bufnr = args.buf
-  vim.diagnostic.open_float({
-    bufnr = bufnr,
-    scope = 'cursor',
-    header = '',
-    source = false,
-    focusable = false,
-    border = 'single',
-    close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' },
-  })
-end
-
-local function lsp_attach(args)
-  local bufnr = args.buf
-  if vim.b[bufnr].user_lsp_diagnostics_enabled or not lsp_utils.buf_supports_method(bufnr, 'textDocument/publishDiagnostics') then
-    -- already seutp or diagnostics not supported
-    return
-  end
-  -- create CursorHold autocommands
-  vim.api.nvim_create_autocmd('CursorHold', { group = augroup, buffer = bufnr, callback = open_float })
-  -- set enabled buffer flag
-  vim.b[bufnr].user_lsp_diagnostics_enabled = true
-end
-
-local function lsp_detach(args)
-  local bufnr = args.buf
-  if lsp_utils.buf_supports_method(bufnr, 'textDocument/publishDiagnostics') then
-    -- some attached client still supports diagnostics
-    return
-  end
-  -- clear buffer autocommands
-  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  -- set disabled buffer flag
-  vim.b[bufnr].user_lsp_diagnostics_enabled = false
-end
-
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
   severity_sort = true,
 })
 
@@ -50,6 +6,3 @@ vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticS
 vim.fn.sign_define('DiagnosticSignWarn',  { text = ' ', texthl = 'DiagnosticSignWarn',  linehl = '', numhl = '' })
 vim.fn.sign_define('DiagnosticSignHint',  { text = ' ', texthl = 'DiagnosticSignHint',  linehl = '', numhl = '' })
 vim.fn.sign_define('DiagnosticSignInfo',  { text = ' ', texthl = 'DiagnosticSignInfo',  linehl = '', numhl = '' })
-
-vim.api.nvim_create_autocmd('LspAttach', { group = augroup, callback = lsp_attach })
-vim.api.nvim_create_autocmd('LspDetach', { group = augroup, callback = lsp_detach })
