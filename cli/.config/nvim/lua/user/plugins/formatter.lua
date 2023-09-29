4567456aa9185120711c7ae58a651e1bf46dc427
Robert Kernan <rkernan@gmail.com>
2023-09-29 04:26:12 +0000

    Auto gofmt and goimports

--- cli/.config/nvim/lua/user/plugins/formatter.lua
+++ cli/.config/nvim/lua/user/plugins/formatter.lua
@@ -6,8 +6,13 @@ return {
       logging = true,
       log_level = vim.log.levels.WARN,
       filetype = {
+        go = {
+          require('formatter.filetypes.go').gofmt,
+          require('formatter.filetypes.go').goimports,
+        },
         python = {
           require('formatter.filetypes.python').isort,
+          -- require('formatter.filetypes.python').black,
         },
         ['*'] = {
           -- require('formatter.filetypes.any').remove_trailing_whitespace,
