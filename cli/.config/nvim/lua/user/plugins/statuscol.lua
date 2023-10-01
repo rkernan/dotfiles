4ed53dea174410b8551c4d384188e69147233a18
Robert Kernan <rkernan@gmail.com>
2023-10-01 16:19:05 +0000

    Added statuscol for cleaner sign column

---
+++ cli/.config/nvim/lua/user/plugins/statuscol.lua
@@ -0,0 +1,16 @@
+return {
+  'luukvbaal/statuscol.nvim',
+  config = function ()
+    local builtin = require('statuscol.builtin')
+    require('statuscol').setup({
+      relculright = true,
+      segments = {
+        { sign = { name = { '.*' }, auto = true }, click = 'v:lua.ScSa' },
+        { sign = { name = { 'Diagnostic.*' }, auto = true }, click = 'v:lua.ScSa' },
+        { sign = { name = { 'GitSigns.*' }, colwidth = 1, auto = true }, click = 'v:lua.ScSa' },
+        { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
+        { text = { ' ', builtin.foldfunc, ' ' }, { builtin.not_empty, true, builtin.not_empty }, click = 'v:lua.ScFa' },
+      }
+    })
+  end
+}
