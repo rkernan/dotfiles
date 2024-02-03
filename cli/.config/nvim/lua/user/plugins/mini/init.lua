9f4cde14de73a7b4db05343df60ffe788a838923
Robert Kernan <rkernan@gmail.com>
2024-02-03 22:48:47 +0000

    Revert "Removed mini.move"
    This reverts commit 8ffa25bacbde195b69e55bfa89e88035e21bc004.

--- cli/.config/nvim/lua/user/plugins/mini/init.lua
+++ cli/.config/nvim/lua/user/plugins/mini/init.lua
@@ -13,6 +13,7 @@ return {
     require('mini.files').setup()
     require('mini.jump').setup()
     require('mini.jump2d').setup()
+    -- require('mini.move').setup()
     require('mini.operators').setup()
     require('mini.pairs').setup()
     require('mini.surround').setup()
