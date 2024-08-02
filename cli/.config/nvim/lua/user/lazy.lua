b38b8f86742be425aec723007307101355280aac
Robert Kernan <rkernan@gmail.com>
2024-08-02 22:35:05 +0000

    Disabled lazy ui backdrop

--- cli/.config/nvim/lua/user/lazy.lua
+++ cli/.config/nvim/lua/user/lazy.lua
@@ -11,4 +11,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('user.plugins')
+require('lazy').setup({
+  spec = {
+    { import = 'user.plugins' },
+  },
+  ui = {
+    backdrop = 100,
+  }
+})
