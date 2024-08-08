ce8a07fc6103bc2e2709294fc359a6688ef46123
Robert Kernan <rkernan@gmail.com>
2024-08-08 16:25:20 +0000

    Added autocmd to restore cursor position on enter

---
+++ cli/.config/nvim/lua/user/autocmds.lua
@@ -0,0 +1,13 @@
+local augroup = vim.api.nvim_create_augroup('user.core', { clear = true })
+
+-- resize windows automatically
+vim.api.nvim_create_autocmd('VimResized', { group = augroup, command = 'wincmd =' })
+
+-- restore last cursor position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup,
+  pattern = { '*' },
+  callback = function ()
+    vim.cmd([[silent! normal! g`"zv]])
+  end
+})
--- cli/.config/nvim/lua/user/events.lua
+++
@@ -1,4 +0,0 @@
-local augroup = vim.api.nvim_create_augroup('core', { clear = true })
-
--- resize windows automatically
-vim.api.nvim_create_autocmd('VimResized', { group = augroup, command = 'wincmd =' })
