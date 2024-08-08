71831aca0ba781a9bebc6d91e68ab20789bd3627
Robert Kernan <rkernan@gmail.com>
2024-08-08 14:42:11 +0000

    Set pycodestyle severity to INFO

--- cli/.config/nvim/lua/user/plugins/lint.lua
+++ cli/.config/nvim/lua/user/plugins/lint.lua
@@ -1,13 +1,26 @@
 return {
   'mfussenegger/nvim-lint',
-  event = { 'BufWritePre', 'BufReadPre' },
+  event = { 'BufReadPre', 'BufWritePre' },
   config = function ()
-    require('lint').linters_by_ft = {
+    local lint = require('lint')
+    local util = require('lint.util')
+
+    lint.linters.pycodestyle = util.wrap(lint.linters.pycodestyle, function (diagnostic)
+      diagnostic.severity = vim.diagnostic.severity.INFO
+      return diagnostic
+    end)
+
+    lint.linters_by_ft = {
       python = {
         'pycodestyle',
         'pydocstyle',
       },
     }
-    vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, { callback = function () require('lint').try_lint() end })
+
+    vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, {
+      callback = function ()
+        require('lint').try_lint()
+      end,
+    })
   end
 }
