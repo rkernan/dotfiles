4d9a254e192550de5ce1471dc680e87d5ffeab1e
Robert Kernan <rkernan@gmail.com>
2024-08-14 13:34:46 +0000

    Fixed foldcolumn padding

--- cli/.config/nvim/lua/user/plugins/heirline/statuscolumn.lua
+++ cli/.config/nvim/lua/user/plugins/heirline/statuscolumn.lua
@@ -19,14 +19,16 @@ return {
     end,
   }, {
     condition = conditions.is_not_active,
-    -- TODO separate gitsigns, diagnostics, and dap
     components.column.folds,
-    utils.surround({ '%=', '' }, nil, components.column.lnum),
-    utils.surround({ '%=', '' }, nil, components.column.signs),
+    utils.surround({ '%=', '' }, nil, components.column.line_number),
+    -- TODO separate gitsigns, diagnostics, and dap
+    components.column.signs,
+    -- utils.surround({ '%=', '' }, nil, components.column.sign_from_extmark('gitsigns_signs')),
   }, {
-    -- TODO separate gitsigns, diagnostics, and dap
     components.column.folds,
-    utils.surround({ '%=', '' }, nil, components.column.lnum),
-    utils.surround({ '%=', '' }, nil, components.column.signs),
+    utils.surround({ '%=', '' }, nil, components.column.line_number),
+    -- TODO separate gitsigns, diagnostics, and dap
+    components.column.signs,
+    -- utils.surround({ '%=', '' }, nil, components.column.sign_from_extmark('gitsigns_signs')),
   }
 }
