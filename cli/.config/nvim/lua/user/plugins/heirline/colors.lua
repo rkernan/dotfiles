602c42c0392d44cd220741048fcb1fccf758d9bc
Robert Kernan <rkernan@gmail.com>
2024-08-17 17:12:05 +0000

    Defined heirline colors for 16 terminal colors

--- cli/.config/nvim/lua/user/plugins/heirline/colors.lua
+++ cli/.config/nvim/lua/user/plugins/heirline/colors.lua
@@ -2,6 +2,23 @@ local utils = require('heirline.utils')
 
 local function setup_colors()
   return {
+    black          = vim.g.terminal_color_0,
+    red            = vim.g.terminal_color_1,
+    green          = vim.g.terminal_color_2,
+    yellow         = vim.g.terminal_color_3,
+    blue           = vim.g.terminal_color_4,
+    magenta        = vim.g.terminal_color_5,
+    cyan           = vim.g.terminal_color_6,
+    white          = vim.g.terminal_color_7,
+    bright_black   = vim.g.terminal_color_8,
+    bright_red     = vim.g.terminal_color_9,
+    bright_green   = vim.g.terminal_color_10,
+    bright_yellow  = vim.g.terminal_color_11,
+    bright_blue    = vim.g.terminal_color_12,
+    bright_magenta = vim.g.terminal_color_13,
+    bright_cyan    = vim.g.terminal_color_14,
+    bright_white   = vim.g.terminal_color_15,
+    -- specials
     dim           = utils.get_highlight('Conceal').fg,
     file_modified = vim.g.terminal_color_2, -- green
     recording_fg  = vim.g.terminal_color_0, -- black
