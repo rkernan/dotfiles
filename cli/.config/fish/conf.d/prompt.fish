1aa8d21079ba835d78bdcbdf0bdfbbc75327a568
Robert Kernan <rkernan@gmail.com>
2024-08-21 21:03:55 +0000

    Added background to fish prompt

--- cli/.config/fish/conf.d/prompt.fish
+++ cli/.config/fish/conf.d/prompt.fish
@@ -1,9 +1,10 @@
 status is-interactive || exit
 
-set fish_color_cwd brwhite
-set fish_color_venv cyan
-set fish_color_mode normal
-set fish_color_jobs yellow
+set fish_prompt_bg_color 3E3E3E
+
+set fish_prompt_color_pwd brwhite
+set fish_prompt_color_venv cyan
+set fish_prompt_color_jobs yellow
 
 # command colors
 set fish_color_command normal
@@ -13,19 +14,19 @@ set fish_color_param normal
 
 # git prompt colors and symbols
 set -g __fish_git_prompt_show_informative_status true
-set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_color_branch magenta --background $fish_prompt_bg_color
 set -g __fish_git_prompt_char_stateseparator ""
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
 set -g __fish_git_prompt_char_upstream_prefix ""
-set -g __fish_git_prompt_color_cleanstate green
+set -g __fish_git_prompt_color_cleanstate green --background $fish_prompt_bg_color
 set -g __fish_git_prompt_char_cleanstate ""
-set -g __fish_git_prompt_color_stagedstate green
+set -g __fish_git_prompt_color_stagedstate green --background $fish_prompt_bg_color
 set -g __fish_git_prompt_char_stagedstate "+"
-set -g __fish_git_prompt_color_dirtystate yellow
+set -g __fish_git_prompt_color_dirtystate yellow --background $fish_prompt_bg_color
 set -g __fish_git_prompt_char_dirtystate "+"
-set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_invalidstate red --background $fish_prompt_bg_color
 set -g __fish_git_prompt_char_invalidstate "⨯"
-set -g __fish_git_prompt_color_untrackedfiles red
+set -g __fish_git_prompt_color_untrackedfiles red --background $fish_prompt_bg_color
 set -g __fish_git_prompt_char_untrackedfiles "…"
--- cli/.config/fish/functions/fish_prompt.fish
+++ cli/.config/fish/functions/fish_prompt.fish
@@ -1,27 +1,24 @@
 function _prompt_jobs
   set -l num_jobs (jobs | wc -l | tr -d '[:space:]')
   if test $num_jobs -gt 0
-    set_color --bold $fish_color_jobs
+    set_color --bold $fish_prompt_color_jobs
     echo -n "$num_jobs "
-    set_color normal
   end
 end
 
 function _prompt_last_pipestatus -a last_pipestatus
-  __fish_print_pipestatus "" " " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus
+  __fish_print_pipestatus '' ' ' '|' (set_color $fish_color_status --background $fish_prompt_bg_color) (set_color --bold $fish_color_status --background $fish_prompt_bg_color) $last_pipestatus
 end
 
 function _prompt_git
   set_color $__fish_git_prompt_color_branch
-  echo -ns (fish_git_prompt ' %s ')
-  set_color normal
+  echo -ns (fish_git_prompt ' %s'(set_color --background $fish_prompt_bg_color)' ')
 end
 
 function _prompt_virtualenv
   if set -q VIRTUAL_ENV
-    set_color $fish_color_venv
+    set_color $fish_prompt_color_venv
     echo -n ' '(basename "$VIRTUAL_ENV")' '
-    set_color normal
   end
 end
 
@@ -29,42 +26,35 @@ function _prompt_hostname
   if set -q SSH_CLIENT || set -q SSH_TTY
     set_color $fish_color_host_remote
     prompt_hostname
-    set_color normal
     echo -n ':'
   end
 end
 
 function _prompt_pwd
-  set_color $fish_color_cwd
-  echo -n ' '
-  prompt_pwd
-  set_color normal
+  set_color $fish_prompt_color_pwd
+  echo -n ' '(prompt_pwd)' '
 end
 
 function _prompt_mode
-  set_color $fish_color_mode
   switch $fish_bind_mode
-    case default
-      echo ' < '
-    case insert
-      echo ' > '
-    case replace_one
-      echo ' > '
-    case replace
-      echo ' < '
-    case visual
-      echo ' < '
+    case insert replace_one
+      echo (set_color normal)' '
+    case default replace visual
+      echo (set_color normal)(set_color --bold red)':'
   end
-  set_color normal
 end
 
 function fish_prompt
   set -l last_pipestatus $pipestatus
-  echo -ns (_prompt_jobs)
-  echo -ns (_prompt_last_pipestatus $last_pipestatus)
-  echo -ns (_prompt_git)
-  echo -ns (_prompt_virtualenv)
-  echo -ns (_prompt_hostname)
-  echo -ns (_prompt_pwd)
-  echo -ns (_prompt_mode)
+  echo -ns                                         \
+    (set_color --background $fish_prompt_bg_color) \
+    ' '                                            \
+    (_prompt_jobs)                                 \
+    (_prompt_last_pipestatus $last_pipestatus)     \
+    (_prompt_git)                                  \
+    (_prompt_virtualenv)                           \
+    (_prompt_hostname)                             \
+    (_prompt_pwd)                                  \
+    (_prompt_mode)                                 \
+    (set_color normal)
 end
