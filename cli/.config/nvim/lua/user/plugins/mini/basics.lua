563ae76e83ee608b74e6afaeb88b26da9fa550e2
Robert Kernan <rkernan@gmail.com>
2024-08-26 13:50:51 +0000

    Don't show listchars by default

--- cli/.config/nvim/lua/user/plugins/mini/basics.lua
+++ cli/.config/nvim/lua/user/plugins/mini/basics.lua
@@ -15,6 +15,7 @@ local function setup()
   -- additional listchars
   vim.o.showbreak = '⌙'
   vim.o.listchars = 'eol:¬,tab:»·,nbsp:␣,trail:·,extends:→,precedes:←'
+  vim.o.list = false
   -- restore <C-z>
   vim.keymap.set('n', '<C-z>', '<Cmd>stop<CR>')
 end
