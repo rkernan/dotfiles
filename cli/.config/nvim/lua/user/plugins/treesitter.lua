bb44dfcd728896f6ac10ff8d3ae3587c5edd0ab2
Robert Kernan <rkernan@gmail.com>
2024-10-21 13:39:54 +0000

    feat: Auto-install tresitter modules as needed

--- cli/.config/nvim/lua/user/plugins/treesitter.lua
+++ cli/.config/nvim/lua/user/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
   },
   config = function ()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = 'all',
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
       endwise = { enable = true },
