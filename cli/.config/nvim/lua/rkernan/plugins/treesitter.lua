2a3ded6f2728b02a3e63e9aa3baca53aa1448b3b
Robert Kernan <rkernan@gmail.com>
2024-10-23 20:31:03 +0000

    tweak: Include ts-comments in treesitter plugin file

--- cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
+++ cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
@@ -1,19 +1,24 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufNewFile', 'BufReadPost' },
-  build = ':TSUpdate',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    'RRethy/nvim-treesitter-endwise',
-  },
-  config = function ()
-    require('nvim-treesitter.configs').setup({
-      auto_install = true,
-      highlight = { enable = true },
-      incremental_selection = { enable = true },
-      indent = { enable = true },
-      endwise = { enable = true },
-      additional_vim_regex_highlighting = false,
-    })
-  end,
+  {
+    'nvim-treesitter/nvim-treesitter',
+    event = { 'BufNewFile', 'BufReadPost' },
+    build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'RRethy/nvim-treesitter-endwise',
+    },
+    config = function ()
+      require('nvim-treesitter.configs').setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        endwise = { enable = true },
+        additional_vim_regex_highlighting = false,
+      })
+    end,
+  }, {
+    'folke/ts-comments.nvim',
+    event = 'VeryLazy',
+    config = true,
+  }
 }
