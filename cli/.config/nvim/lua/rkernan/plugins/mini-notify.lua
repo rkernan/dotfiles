9e22ba31de0a00d385c3e77fbc897f85a89bb0c7
Robert Kernan <rkernan@gmail.com>
2024-11-01 16:09:49 +0000

    tweak: Only specify INFO hl_group change

--- cli/.config/nvim/lua/rkernan/plugins/mini-notify.lua
+++ cli/.config/nvim/lua/rkernan/plugins/mini-notify.lua
@@ -12,13 +12,6 @@ return {
     })
 
     -- override vim.notify
-    vim.notify = MiniNotify.make_notify({
-      ERROR = { duration = 5000, hl_group = 'DiagnosticError'  },
-      WARN  = { duration = 5000, hl_group = 'DiagnosticWarn'   },
-      INFO  = { duration = 5000, hl_group = 'MiniNotifyNormal' },
-      DEBUG = { duration = 0,    hl_group = 'DiagnosticHint'   },
-      TRACE = { duration = 0,    hl_group = 'DiagnosticOk'     },
-      OFF   = { duration = 0,    hl_group = 'MiniNotifyNormal' },
-    })
+    vim.notify = MiniNotify.make_notify({ INFO = { hl_group = 'MiniNotifyNormal' }})
   end,
 }
