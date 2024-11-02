f33e743e576f390aafe77638d74c01611ba6ae4d
Robert Kernan <rkernan@gmail.com>
2024-11-02 19:22:51 +0000

    feat: Removed unused MiniNotify{Enable,Disable}Debug user commands

--- cli/.config/nvim/lua/rkernan/plugins/mini-notify.lua
+++ cli/.config/nvim/lua/rkernan/plugins/mini-notify.lua
@@ -12,18 +12,6 @@ return {
     })
 
     -- override vim.notify
-    local notify_opts = { INFO = { hl_group = 'MiniNotifyNormal' }}
     vim.notify = MiniNotify.make_notify({ INFO = { hl_group = 'MiniNotifyNormal' }})
-
-    vim.api.nvim_create_user_command('MiniNotifyEnableDebug',
-      function ()
-        vim.notify = MiniNotify.make_notify(vim.tbl_extend('force', notify_opts, { DEBUG = { duration = 5000 }}))
-      end,
-      { nargs = 0 })
-    vim.api.nvim_create_user_command('MiniNotifyDisableDebug',
-      function ()
-        vim.notify = MiniNotify.make_notify(vim.tbl_extend('force', notify_opts, { DEBUG = { duration = 0 }}))
-      end,
-      { nargs = 0 })
   end,
 }
