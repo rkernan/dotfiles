4f3bcd20ad2cf83b9c9ae77391aec353f954dfdd
Robert Kernan <rkernan@gmail.com>
2024-11-04 15:17:57 +0000

    fix: Refresh git.head component on GitHeadUpdate without breaking bg colors

--- cli/.config/nvim/lua/rkernan/plugins/heirline/components/git.lua
+++ cli/.config/nvim/lua/rkernan/plugins/heirline/components/git.lua
@@ -1,18 +1,24 @@
 local M = {}
 
+local utils = require('rkernan.plugins.heirline.utils')
+
 M.head = {
   static = {
     icon = '',
   },
+  init = function (self)
+    if not rawget(self, 'once') then
+      vim.api.nvim_create_autocmd('User', { pattern = 'GitHeadUpdate', callback = function () utils.reset_win_cache(self) end })
+      self.once = true
+    end
+  end,
   condition = function ()
     return vim.g.git_head
   end,
-  -- TODO
   provider = function (self)
     return string.format('%s %s', self.icon, vim.g.git_head)
   end,
   hl = { fg = 'git_head' },
-  update = { 'User', pattern = 'GitHeadUpdate' },
 }
 
 return M
