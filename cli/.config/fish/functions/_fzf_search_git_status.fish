5bbc0306cfbbeb6e3f85696a67fa78faeafe9df7
Robert Kernan <rkernan@gmail.com>
2024-11-11 14:45:35 +0000

    fix: Return correct git-status result when entry is a rename
    <STATUS> <ORIGINAL_NAME> -> <NEW_NAME>

--- cli/.config/fish/functions/_fzf_search_git_status.fish
+++ cli/.config/fish/functions/_fzf_search_git_status.fish
@@ -3,9 +3,16 @@ function _fzf_search_git_status
     return
   end
 
-  git -c color.status=always status --short | _fzf_wrapper --prompt='GitStatus> ' --preview='git diff --color HEAD -- {-1}' --query=(commandline -t) -m --ansi | awk '{print $2}' |
+  git -c color.status=always status --short | _fzf_wrapper --prompt='GitStatus> ' --preview='git diff --color HEAD -- {-1}' --query=(commandline -t) -m --ansi |
     while read -l r
-      set result $result (string escape $r)
+      set --local split (string split --no-empty ' ' $r)
+      if test (count $split) -eq 4
+      and test "$split[3]" = "->"
+        # moved: <STATUS> <NAME> -> <NEW_NAME>
+        set result $result (string escape $split[4])
+      else
+        set result $result (string escape $split[2])
+      end
     end
 
   commandline -rt -- (string join ' ' $result)
