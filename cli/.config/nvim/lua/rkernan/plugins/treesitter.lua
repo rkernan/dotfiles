2545142e35575b5fe16bb83642656f708b3a82b1
Robert Kernan <rkernan@gmail.com>
2025-02-26 11:15:57 -0500

    tweak: Removed unneeded @diagnostic disable-next-line

--- cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
+++ cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
@@ -8,7 +8,6 @@ return {
       'RRethy/nvim-treesitter-endwise',
     },
     config = function ()
-      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup({
         auto_install = true,
         highlight = { enable = true },
