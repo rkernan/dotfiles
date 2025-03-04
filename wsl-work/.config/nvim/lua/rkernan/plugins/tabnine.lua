5b1aa21c58d8d6734ee9d8c161bf78cb745b8291
Robert Kernan <rkernan@gmail.com>
2025-03-04 03:55:00 +0000

    Disable tabnine codelens

--- wsl-work/.config/nvim/lua/rkernan/plugins/tabnine.lua
+++ wsl-work/.config/nvim/lua/rkernan/plugins/tabnine.lua
@@ -9,6 +9,7 @@ return {
     tabnine_enterprise_host = os.getenv("TABNINE_ENTERPRISE_HOST") or nil,
     disable_auto_comment = true,
     accept_keymap = '<C-f>',
+    codelens_enabled = false,
   },
   config = function (_, opts)
     require('tabnine').setup(opts)
