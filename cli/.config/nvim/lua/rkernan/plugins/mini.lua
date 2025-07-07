168a75c052e2bc2bb6606e2c8015632127f01f8d
Robert Kernan <rkernan@gmail.com>
2025-07-07 17:16:08 +0000

    fix: use standard load path for mini.deps

--- cli/.config/nvim/lua/rkernan/plugins/mini.lua
+++ cli/.config/nvim/lua/rkernan/plugins/mini.lua
@@ -1,5 +1,5 @@
 local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/start/mini.nvim'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installing `mini.nvim`" | redraw')
   local clone_cmd = {
