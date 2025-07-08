68f1aedec9f2e90a2d5f9b81518fac0614aa2df4
Robert Kernan <rkernan@gmail.com>
2025-07-08 10:03:31 -0400

    tweak: reordered plugin execution

--- cli/.config/nvim/lua/rkernan/pack.lua
+++ cli/.config/nvim/lua/rkernan/pack.lua
@@ -1,14 +1,17 @@
 require('rkernan.plugins.mini')
 
+require('rkernan.plugins.middleclass')
+
 require('rkernan.plugins.blink-cmp')
 require('rkernan.plugins.conform')
 require('rkernan.plugins.faster')
 require('rkernan.plugins.guess-indent')
-require('rkernan.plugins.heirline')
 require('rkernan.plugins.lint')
 require('rkernan.plugins.lspconfig')
-require('rkernan.plugins.middleclass')
 require('rkernan.plugins.treesitter')
-require('rkernan.plugins.zenbones')
+
 -- try to load tabnine
 pcall(require, 'rkernan.plugins.tabnine')
+
+require('rkernan.plugins.heirline')
+require('rkernan.plugins.zenbones')
