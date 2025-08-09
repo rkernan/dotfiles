2e2b7d68ac4a40c60dc71a84adc8a46f9244efd5
Robert Kernan <rkernan@gmail.com>
2025-08-09 16:07:15 -0400

    feat!: switched to built-in lsp keymaps

--- cli/.config/nvim/lua/rkernan/plugins/lspconfig.lua
+++ cli/.config/nvim/lua/rkernan/plugins/lspconfig.lua
@@ -8,45 +8,15 @@ local function lsp_attach(args)
 
   vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
 
-  vim.keymap.set('n', 'K', function () vim.lsp.buf.hover({ border = 'single' }) end, { buffer = bufnr, desc = 'LSP hover' })
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'LSP definition' })
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = 'LSP declaration' })
-  -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = bufnr, desc = 'LSP implementation' })
-  vim.keymap.set('n', 'gi', function () require('mini.extra').pickers.lsp({ scope = 'implementation' }) end, { buffer = bufnr, desc = 'LSP implementation' })
-  vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { buffer = bufnr, desc = 'LSP definition' })
-  -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = 'LSP references' })
-  vim.keymap.set('n', 'gr', function () require('mini.extra').pickers.lsp({ scope = 'references' }) end, { buffer = bufnr, desc = 'LSP references' })
-  -- vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'LSP signature help' })
-  vim.keymap.set('n', '<Leader><Leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = 'LSP buffer rename' })
-  vim.keymap.set({'n', 'x'}, '<Leader><Leader>f', function () vim.lsp.buf.format({async = true}) end, { buffer = bufnr, desc = 'LSP format' })
-  vim.keymap.set('n', '<Leader><Leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = 'LSP code action' })
-
-  -- symbols
-  vim.keymap.set('n', '<Leader><Leader>o', function () require('mini.extra').pickers.lsp({ scope = 'document_symbol' }) end, { buffer = bufnr, desc = 'LSP document symbols' })
-  vim.keymap.set('n', '<Leader><Leader>O', function () require('mini.extra').pickers.lsp({ scope = 'workspace_symbol' }) end, { buffer = bufnr, desc = 'LSP workspace symbols' })
-
-  -- toggle inlay hints
-  vim.keymap.set('n', '<Leader><Leader>i',
-    function () vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
-    { buffer = bufnr, desc = 'LSP toggle inlay hints'}
-  )
+  vim.keymap.set({ 'i', 'v', }, '<C-s>', function () vim.lsp.buf.hover({ border = 'single' }) end, { buffer = bufnr, desc = 'LSP hover' })
+  vim.keymap.set('n', 'S', function () vim.lsp.buf.hover({ border = 'single' }) end, { buffer = bufnr, desc = 'LSP hover' })
+  vim.keymap.set('n', 'grr', function () require('mini.extra').pickers.lsp({ scope = 'references' }) end, { buffer = bufnr, desc = 'LSP references' })
+  vim.keymap.set('n', 'gO', function () require('mini.extra').pickers.lsp({ scope = 'document_symbol' }) end, { buffer = bufnr, desc = 'LSP document symbols' })
 end
 
 local function lsp_detach(args)
   local bufnr = args.buf
-  pcall(vim.keymap.del, 'n', 'K', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', 'gd', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', 'gD', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', 'gi', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', 'go', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', 'gr', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', 'gs', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', '<Leader><Leader>rn', { buffer = bufnr })
-  pcall(vim.keymap.del, {'n', 'x'}, '<Leader><Leader>f', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', '<Leader><Leader>ca', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', '<Leader><Leader>o', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', '<Leader><Leader>O', { buffer = bufnr })
-  pcall(vim.keymap.del, 'n', '<Leader><Leader>i', { buffer = bufnr })
+  pcall(vim.keymap.del, 'n', 'S', { buffer = bufnr })
 end
 
 ---@diagnostic disable: undefined-global
