6dcc6f6de4bffab0a223ab2e4c657b2fa65c8aa5
Robert Kernan <rkernan@gmail.com>
2025-08-14 22:20:39 -0400

    feat!: removed treesitter-endwise

--- cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
+++ cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
@@ -5,12 +5,10 @@ local add, now = MiniDeps.add, MiniDeps.now
 now(function ()
   add({ source = 'nvim-treesitter/nvim-treesitter' })
   add({ source = 'nvim-treesitter/nvim-treesitter-textobjects', depends = { 'nvim-treesitter/nvim-treesitter' } })
-  add({ source = 'RRethy/nvim-treesitter-endwise', depends = { 'nvim-treesitter/nvim-treesitter' } })
   require('nvim-treesitter.configs').setup({
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
-    endwise = { enable = true },
     additional_vim_regex_highlighting = false,
   })
 end)
