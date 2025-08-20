cc4c0c7e55beaec1dc6bb793980ba4ea6ff3549d
Robert Kernan <rkernan@gmail.com>
2025-08-20 14:25:11 -0400

    feat!: replaced floating-terminal with simple tab-based persistent terminal mapping

--- cli/.config/nvim/plugin/keymaps.lua
+++ cli/.config/nvim/plugin/keymaps.lua
@@ -8,3 +8,16 @@ vim.keymap.set('x', '>', '>gv', { desc = 'Increase indent' })
 vim.keymap.set('x', '<', '<gv', { desc = 'Decrease indent' })
 vim.keymap.set('n', '<C-e>', function () vim.diagnostic.open_float() end, { desc = 'Open diagnostic float' })
 vim.keymap.set('n', '<C-w>z', '<C-w>_<C-w>|', { desc = 'Zoom current window' })
+
+vim.keymap.set('n', '<Leader>t', function ()
+  if vim.g.terminal_tab then
+    vim.cmd(string.format('tabnext %d | startinsert', vim.g.terminal_tab))
+    return
+  end
+  vim.cmd('tabnew | startinsert | terminal')
+  local index = vim.fn.tabpagenr()
+  vim.g.terminal_tab = index
+  local bufnr = vim.fn.tabpagebuflist(index)[vim.fn.tabpagewinnr(index)]
+  vim.keymap.set('t', '<A-i>', function () vim.cmd('tabnext #') end, { buffer = bufnr })
+  vim.api.nvim_create_autocmd('TabClosed', { buffer = bufnr, callback = function () vim.g.terminal_tab = nil end })
+end, { desc = 'Persistent terminal' })
