279bcf560b60696795c84cf796ea067c945a7173
Robert Kernan <rkernan@gmail.com>
2025-08-21 14:28:36 +0000

    feat: simplified fish prompt

--- cli/.config/fish/functions/fish_prompt.fish
+++ cli/.config/fish/functions/fish_prompt.fish
@@ -7,12 +7,12 @@ function _prompt_jobs
 end
 
 function _prompt_last_pipestatus -a last_pipestatus
-  __fish_print_pipestatus '' ' ' '|' (set_color $fish_color_status --background $fish_prompt_bg_color) (set_color --bold $fish_color_status --background $fish_prompt_bg_color) $last_pipestatus
+  __fish_print_pipestatus '' ' ' '|' (set_color red) (set_color --bold red) $last_pipestatus
 end
 
 function _prompt_git
   set_color $__fish_git_prompt_color_branch
-  echo -ns (fish_git_prompt ' %s'(set_color --background $fish_prompt_bg_color)' ')
+  echo -ns (fish_git_prompt ' %s ')
 end
 
 function _prompt_virtualenv
@@ -32,29 +32,18 @@ end
 
 function _prompt_pwd
   set_color $fish_prompt_color_pwd
-  echo -n ' '(prompt_pwd)' '
-end
-
-function _prompt_mode
-  switch $fish_bind_mode
-    case insert replace_one
-      echo (set_color normal)' '
-    case default replace visual
-      echo (set_color normal)(set_color --bold red)':'
-  end
+  echo -n (prompt_pwd)' '
 end
 
 function fish_prompt
   set -l last_pipestatus $pipestatus
-  echo -ns                                         \
-    (set_color --background $fish_prompt_bg_color) \
-    ' '                                            \
+  echo -s                                          \
+    '┌─ '                                            \
     (_prompt_jobs)                                 \
     (_prompt_last_pipestatus $last_pipestatus)     \
     (_prompt_git)                                  \
     (_prompt_virtualenv)                           \
     (_prompt_hostname)                             \
-    (_prompt_pwd)                                  \
-    (_prompt_mode)                                 \
-    (set_color normal)
+    (_prompt_pwd)
+  echo -s '└→ ' (set_color normal)
 end
