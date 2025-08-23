3a888c861631060579dabc844107c75de349f745
Robert Kernan <rkernan@gmail.com>
2025-08-22 21:33:15 -0400

    feat!: remove unused treesitter-textobjects plugin

--- cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
+++ cli/.config/nvim/lua/rkernan/plugins/treesitter.lua
@@ -3,7 +3,6 @@ local add = MiniDeps.add
 ---@diagnostic enable: undefined-global
 
 add({ source = 'nvim-treesitter/nvim-treesitter' })
-add({ source = 'nvim-treesitter/nvim-treesitter-textobjects', depends = { 'nvim-treesitter/nvim-treesitter' } })
 require('nvim-treesitter.configs').setup({
   auto_install = true,
   highlight = { enable = true },
