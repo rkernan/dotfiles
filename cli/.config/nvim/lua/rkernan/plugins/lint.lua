e94c7e59695fbe6a03c993b764afc56d0cc8f7f5
Robert Kernan <rkernan@gmail.com>
2025-08-25 21:14:20 -0400

    fix(nvim): disable linting by default

--- cli/.config/nvim/lua/rkernan/plugins/lint.lua
+++ cli/.config/nvim/lua/rkernan/plugins/lint.lua
@@ -3,6 +3,7 @@ local add = MiniDeps.add
 ---@diagnostic enable: undefined-global
 
 add({ source = 'mfussenegger/nvim-lint' })
+require('lint').linters_by_ft = {}
 
 local augroup = vim.api.nvim_create_augroup('rkernan.lint', { clear = true })
 vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, { callback = function () require('lint').try_lint() end, group = augroup })
