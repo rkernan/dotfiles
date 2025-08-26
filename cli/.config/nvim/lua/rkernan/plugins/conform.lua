d88a561c182fded50efa797870de63d65a2e908c
Robert Kernan <rkernan@gmail.com>
2025-08-25 21:26:25 -0400

    feat(nvim): conform fallback to lsp formatter

--- cli/.config/nvim/lua/rkernan/plugins/conform.lua
+++ cli/.config/nvim/lua/rkernan/plugins/conform.lua
@@ -7,5 +7,8 @@ require('conform').setup({
   formatters_by_ft = {
     ['*'] = { 'trim_whitespace' },
   },
-  format_on_save = { timeout_ms = 500 }
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = 'fallback',
+  },
 })
