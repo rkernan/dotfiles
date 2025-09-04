c702ca19f163c5f3c317857fc9b3c6aa76ea880b
Robert Kernan <rkernan@gmail.com>
2025-09-04 15:59:24 -0400

    tweak(nvim): basic signcolumn for quickfix

--- cli/.config/nvim/after/ftplugin/qf.lua
+++ cli/.config/nvim/after/ftplugin/qf.lua
@@ -1,3 +1,5 @@
 vim.opt_local.statusline = ''
-vim.opt_local.number = false
+vim.opt_local.number = true
 vim.opt_local.relativenumber = false
+vim.opt_local.statuscolumn = '%=%l '
+vim.opt_local.signcolumn = 'no'
