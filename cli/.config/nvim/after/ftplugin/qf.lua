ecefafa78ac9df50b94795d72bc54968ae195cd1
Robert Kernan <rkernan@gmail.com>
2025-09-05 00:50:55 -0400

    feat(nvim): use 'q' to close qf window

--- cli/.config/nvim/after/ftplugin/qf.lua
+++ cli/.config/nvim/after/ftplugin/qf.lua
@@ -3,3 +3,4 @@ vim.opt_local.number = true
 vim.opt_local.relativenumber = false
 vim.opt_local.statuscolumn = '%=%l '
 vim.opt_local.signcolumn = 'no'
+vim.keymap.set('n', 'q', ':q<Enter>', { silent = true })
