f5b2baf6da61149a12770f43c0c7794d5f95fd48
Robert Kernan <rkernan@gmail.com>
2026-02-11 08:37:07 -0500

    tweak: only display wezterm tabs if there ismore than one

--- gui/.config/wezterm/wezterm.lua
+++ gui/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require('wezterm')
+
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
@@ -6,6 +7,8 @@ config.font_size = 10.5
 config.color_scheme = 'gruvbones-dark'
 
 config.default_domain = os.getenv('WEZTERM_DEFAULT_DOMAIN') or 'local'
+
+config.hide_tab_bar_if_only_one_tab = true
 
 config.disable_default_key_bindings = true
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
