9754ef0b200341dbc933df54c61e94493f99fa83
Robert Kernan <rkernan@gmail.com>
2026-02-21 10:30:34 -0500

    fix: set FZF_CTRL_T_COMMAND to use _list_files for default key-bindings]

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -63,6 +63,7 @@ end
 # fzf config
 set -x FZF_DEFAULT_OPTS "--cycle --layout=reverse --style=minimal --info=inline --height=20"
 set -x FZF_DEFAULT_COMMAND "_list_files \$dir"
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -x FZF_CTRL_T_OPTS "--prompt='files> '"
 set -x FZF_CTRL_R_OPTS "--prompt='history> ' --no-multi"
 set -x FZF_ALT_C_COMMAND "_list_dirs \$dir"
