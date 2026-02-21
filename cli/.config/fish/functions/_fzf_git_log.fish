65218f80db6edaac7b2d68aabe110690a88d2e02
Robert Kernan <rkernan@gmail.com>
2026-02-21 10:33:41 -0500

    tweak: fixed formatting in _fzf_git_log

--- cli/.config/fish/functions/_fzf_git_log.fish
+++ cli/.config/fish/functions/_fzf_git_log.fish
@@ -4,7 +4,8 @@ function _fzf_git_log
   end
 
   git log --color |
-  fzf --height=100% --prompt='git-log> ' --query=(commandline -t) -m --ansi | awk '{print $1}' |
+  fzf --height=100% --prompt='git-log> ' --query=(commandline -t) -m --ansi |
+  awk '{print $1}' |
   while read -l r
     set -f result $result (git rev-parse (string escape $r))
   end
