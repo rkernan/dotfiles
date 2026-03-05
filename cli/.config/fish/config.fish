92721c5443c1d700537864160ce366e5cc74d6b7
Robert Kernan <rkernan@gmail.com>
2026-03-05 10:30:28 -0500

    tweak: don't include config files in zoxide

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -71,6 +71,7 @@ set -x FZF_ALT_C_OPTS "--prompt='dirs> '"
 
 # zoxide config
 zoxide init fish | source
+set -x _ZO_EXCLUDE_DIRS (string join ':' $HOME $HOME"/.*")
 set -x _ZO_FZF_OPTS $FZF_DEFAULT_OPTS "--height=10 --prompt='zoxide> ' --no-multi"
 
 # ripgrep config
