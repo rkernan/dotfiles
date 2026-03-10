2ade52a3df691477eaa882905ac7fb6d29dadae7
Robert Kernan <rkernan@gmail.com>
2026-03-10 10:19:46 -0400

    tweak: moved ripgrep config to XDG_CONFIG_HOME

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -75,7 +75,7 @@ set -x _ZO_EXCLUDE_DIRS (string join ':' $HOME $HOME"/.*")
 set -x _ZO_FZF_OPTS $FZF_DEFAULT_OPTS "--height=10 --prompt='zoxide> ' --no-multi"
 
 # ripgrep config
-set -x RIPGREP_CONFIG_PATH $HOME"/.ripgreprc"
+set -x RIPGREP_CONFIG_PATH $HOME"/.config/ripgrep/config"
 
 # aliases
 abbr e $EDITOR
