407c14cd9157b25c8ffcc45e6c91ed253900f544
Robert Kernan <rkernan@gmail.com>
2026-03-29 15:01:24 -0400

    tweak: use individual mini libs instead of full batch

--- cli/dot-config/nvim/plugin/00-pack.lua
+++ cli/dot-config/nvim/plugin/00-pack.lua
@@ -1,14 +1,14 @@
 local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
-if not vim.uv.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+local mini_deps_path = path_package .. 'pack/deps/start/mini.deps'
+if not vim.uv.fs_stat(mini_deps_path) then
+  vim.cmd('echo "Installing `mini.deps`" | redraw')
   local clone_cmd = {
     'git', 'clone', '--filter=blob:none',
-    'https://github.com/nvim-mini/mini.nvim', mini_path
+    'https://github.com/nvim-mini/mini.deps', mini_deps_path
   }
   vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
-  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+  vim.cmd('packadd mini.deps | helptags ALL')
+  vim.cmd('echo "Installed `mini.deps`" | redraw')
 end
 
 require('mini.deps').setup({ path = { package = path_package } })
@@ -17,11 +17,28 @@ require('mini.deps').setup({ path = { package = path_package } })
 local add = MiniDeps.add
 ---@diagnostic enable: undefined-global
 
-add({ name = 'mini.nvim', checkout = 'stable' })
+add({ name = 'mini.deps', checkout = 'stable' })
+
 add({ source = 'mfussenegger/nvim-lint' })
 add({ source = 'mrjones2014/smart-splits.nvim' })
 add({ source = 'neovim/nvim-lspconfig' })
 add({ source = 'nmac427/guess-indent.nvim' })
+add({ source = 'nvim-mini/mini.ai', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.align', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.bracketed', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.cmdline', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.comment', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.completion', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.diff', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.extra', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.hipatterns', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.icons', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.misc', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.notify', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.operators', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.pairs', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.pick', checkout = 'stable' })
+add({ source = 'nvim-mini/mini.surround', checkout = 'stable' })
 add({ source = 'nvim-treesitter/nvim-treesitter' })
 add({ source = 'pteroctopus/faster.nvim' })
 add({ source = 'stevearc/conform.nvim' })
