1810bdf7369d216d9b21caee8ba3694c84d74310
Robert Kernan <rkernan@gmail.com>
2026-04-21 15:49:16 -0400

    tweak: set formatexpr to use conform where applicable

--- cli/dot-config/nvim/after/ftplugin/go.lua
+++ cli/dot-config/nvim/after/ftplugin/go.lua
@@ -1,3 +1,5 @@
 vim.bo.tabstop = 8
 vim.bo.expandtab = false
+
 require('conform').formatters_by_ft.go = { 'goimports', 'gofmt' }
+vim.bo.formatexpr = "v:lua.require'conform'.formatexpr()"
--- cli/dot-config/nvim/after/ftplugin/lua.lua
+++ cli/dot-config/nvim/after/ftplugin/lua.lua
@@ -3,3 +3,4 @@ vim.bo.tabstop = 8
 vim.bo.expandtab = true
 
 require('conform').formatters_by_ft.lua = { 'stylua' }
+vim.bo.formatexpr = "v:lua.require'conform'.formatexpr()"
