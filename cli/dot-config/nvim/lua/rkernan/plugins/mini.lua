09fe9d1f0533f008bbde50499ce37a9cafbb1e3b
Robert Kernan <rkernan@gmail.com>
2026-04-23 11:12:58 -0400

    feat: added mini.clue plugin

--- cli/dot-config/nvim/lua/rkernan/plugins/mini.lua
+++ cli/dot-config/nvim/lua/rkernan/plugins/mini.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   'https://github.com/nvim-mini/mini.ai.git',
   'https://github.com/nvim-mini/mini.align.git',
   'https://github.com/nvim-mini/mini.bracketed.git',
+  'https://github.com/nvim-mini/mini.clue.git',
   'https://github.com/nvim-mini/mini.cmdline.git',
   'https://github.com/nvim-mini/mini.comment.git',
   'https://github.com/nvim-mini/mini.completion.git',
@@ -23,6 +24,42 @@ require('mini.bracketed').setup()
 require('mini.comment').setup()
 require('mini.completion').setup()
 require('mini.cmdline').setup()
+
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = { 'n', 'x' }, keys = '<Leader>' },
+    -- `[` and `]` keys
+    { mode = 'n', keys = '[' },
+    { mode = 'n', keys = ']' },
+    -- Built-in completion
+    { mode = 'i', keys = '<C-x>' },
+    -- `g` key
+    { mode = { 'n', 'x' }, keys = 'g' },
+    -- Marks
+    { mode = { 'n', 'x' }, keys = "'" },
+    { mode = { 'n', 'x' }, keys = '`' },
+    -- Registers
+    { mode = { 'n', 'x' }, keys = '"' },
+    { mode = { 'i', 'c' }, keys = '<C-r>' },
+    -- Window commands
+    { mode = 'n', keys = '<C-w>' },
+    -- `z` key
+    { mode = { 'n', 'x' }, keys = 'z' },
+  },
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.square_brackets(),
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+})
+
 require('mini.operators').setup()
 require('mini.pairs').setup()
 require('mini.surround').setup()
