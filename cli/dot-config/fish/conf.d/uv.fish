783acebee5ddf739b89de652e8386cda3578a6d3
Robert Kernan <rkernan@gmail.com>
2026-04-30 15:02:43 -0400

    tweak: moved uv completions helper to completions/uv.fish

--- cli/dot-config/fish/conf.d/uv.fish
+++ cli/dot-config/fish/conf.d/uv.fish
@@ -1,12 +1,3 @@
-function __fish_uv_run_completions
-  if set -q VIRTUAL_ENV
-    and test -d "$VIRTUAL_ENV/bin"
-    ls -1 "$VIRTUAL_ENV/bin"
-  end
-end
-
-complete -c uv -n '__fish_seen_subcommand_from run; and __fish_is_nth_token 2' -a '(__fish_uv_run_completions)'
-
 function auto_virtualenv --on-event fish_prompt
   set -f virtual_env (find_upwards ".venv")
   set -f activate $virtual_env"/bin/activate.fish"
