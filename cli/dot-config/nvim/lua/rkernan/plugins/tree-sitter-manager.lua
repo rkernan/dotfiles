1c1954d053b9131524ad432b8412e374ec7ab725
Robert Kernan <rkernan@gmail.com>
2026-05-05 09:50:03 -0400

    fix: don't lazy-load tree-sitter-manager

--- cli/dot-config/nvim/lua/rkernan/plugins/tree-sitter-manager.lua
+++ cli/dot-config/nvim/lua/rkernan/plugins/tree-sitter-manager.lua
@@ -2,7 +2,6 @@ return {
   src = 'https://github.com/romus204/tree-sitter-manager.nvim.git',
   name = 'tree-sitter-manager',
   data = {
-    when = 'event:FileType',
     config = {
       auto_install = true,
     },
